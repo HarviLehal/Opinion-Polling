65921e531ff3f519b2519253ab0d9fca6284b78d
Harvi Lehal <115153109+HarviLehal@users.noreply.github.com>
2022-12-03 18:17:56 +0000

    Add files via upload

---
+++ poll.R
@@ -0,0 +1,39 @@
+setwd("C:/Users/harvi/Desktop/Random Stuff/Polling/Polling")
+library(readr)
+poll <- read_csv("poll2.csv")
+library(reshape2)
+d <- melt(poll, id.vars="Date")
+d$Date<-as.Date(d$Date, "%d/%m/%Y")
+library(formattable)
+d$value<-formattable::percent(d$value)
+library(ggplot2)
+library(bbplot)
+library(tidyquant)
+library(scales)
+
+# TWITTER GRAPH
+
+ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=0.5) +
+  scale_color_manual(values = c("#0087DC","#E4003B","#FAA61A","#FDF38E","#528D6B"))+
+  bbplot::bbc_style()+
+  scale_y_continuous(name="Vote",labels = formattable::percent,breaks=seq(0,0.6,0.05))+
+  geom_ma(ma_fun=EMA, n = 5,linetype="solid",size=0.75,wilder=TRUE)
+
+# LOESS GRAPH
+
+ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=0.5) +
+  scale_color_manual(values = c("#0087DC","#E4003B","#FAA61A","#FDF38E","#528D6B"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=TRUE,span=0.3,size=0.75)+
+  bbplot::bbc_style()+
+  scale_y_continuous(name="Vote",labels = formattable::percent,breaks=seq(0,0.6,0.05))
+
+# EXPERIMENTAL THINGS
+
+ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=0.5) +
+  scale_color_manual(values = c("#0087DC","#E4003B","#FAA61A","#FDF38E","#528D6B"))+
+  bbplot::bbc_style()+
+  scale_y_continuous(name="Vote",labels = formattable::percent,breaks=seq(0,0.6,0.05))+
+  geom_ma(ma_fun=EMA, n = 5,linetype="solid",size=0.75,ratio=0.1)
