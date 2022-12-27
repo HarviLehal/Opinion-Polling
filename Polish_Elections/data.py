09012dc08f0fb88b04fb7f5cc01633bbe05f26d9
Harvi Lehal <115153109+HarviLehal@users.noreply.github.com>
2022-12-27 23:15:50 +0000

    Polish Seat Projections

---
+++ Polish_Elections/data.py
@@ -0,0 +1,46 @@
+import pandas as pd # library for data analysis
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+
+wikiurl="https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Polish_parliamentary_election"
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table',class_="wikitable")
+df=pd.read_html(str(tables))
+
+df7=pd.DataFrame(df[7])
+data22 = df7.drop(["Polling Firm/Link", "Sample Size", "Majority", "Kukiz'15"], axis=1)
+headers = ['Date', 'United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
+parties = ['United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
+data22.columns = headers
+data22['Date'] = [x.strip()[-11:] for x in data22['Date']]
+data22['Date'] = [x.replace('–','') for x in data22['Date']]
+for z in parties:
+        data22[z] = [x.replace('[av]','') for x in data22[z]]
+        data22[z] = [x.replace('[aw]','') for x in data22[z]]
+        data22[z] = [x.replace('[t]','') for x in data22[z]]
+        data22[z] = [x.replace('[ax]','') for x in data22[z]]
+
+data22.drop(data22.tail(1).index,inplace=True)
+print(data22)
+
+df8=pd.DataFrame(df[8])
+data21 = df8.drop(["Polling Firm/Link", "Sample Size", "Majority"], axis=1)
+headers = ['Date', 'United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
+parties = ['United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
+data21.columns = headers
+data21['Date'] = [x.strip()[-11:] for x in data21['Date']]
+data21['Date'] = [x.replace('–','') for x in data21['Date']]
+for z in parties:
+        data22[z] = [x.replace('[t]','') for x in data21[z]]
+
+data21.drop(data21.tail(1).index,inplace=True)
+print(data21)
+
+
+
+data = pd.concat([data22,data21])
+print(data)
+data.to_csv('Polish_Elections/poll.csv', index=False)
