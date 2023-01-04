c97f4520b82ea5c04ad8172ccbe1f2afc9a9f3ae
Harvi Lehal <115153109+HarviLehal@users.noreply.github.com>
2023-01-04 21:27:59 +0000

    2023 US Speaker Election (because its funny)

--- US_Elections/Speaker/poll.R
+++ US_Elections/Speaker/poll.R
@@ -14,9 +14,9 @@ plot<-ggplot(data=d,aes(x=Round,y=value, colour=variable, group=variable)) +
   scale_color_manual(values = c("#1b66e8","#E81B23","#941015","#A2A9B1"))+
   bbc_style()+
   geom_line(size=0.5)+
-  geom_segment(aes(x=3,y=h, xend=-Inf, yend=h), alpha=0.75,colour="#000000")+
-  geom_segment(aes(x=5,y=h-1, xend=3, yend=h-1), alpha=0.75,colour="#000000")+
-  geom_segment(aes(x=3,y=h, xend=3, yend=h-1), alpha=0.75,colour="#000000")+
+  geom_segment(aes(x=4,y=h, xend=-Inf, yend=h), alpha=0.75,colour="#000000")+
+  geom_segment(aes(x=max(d$Round),y=h-1, xend=4, yend=h-1), alpha=0.75,colour="#000000")+
+  geom_segment(aes(x=4,y=h, xend=4, yend=h-1), alpha=0.75,colour="#000000")+
   # geom_hline(aes(yintercept=h), alpha=0.75,colour="#000000")+
   geom_text(aes((min(d$Round)),h-2.5,label = "Majority", vjust = -1),colour="#000000")+
   theme(axis.title = element_text(size = 18))+
