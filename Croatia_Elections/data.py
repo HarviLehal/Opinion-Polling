676b011b292e0ac2d6e39da7d894d4164e93c152
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-01-07 00:51:20 +0000

    HRVATSKA!!!

---
+++ Croatia_Elections/data.py
@@ -0,0 +1,39 @@
+import pandas as pd # library for data analysis
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+
+wikiurl="https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Croatian_parliamentary_election"
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table',class_="wikitable")
+df=pd.read_html(str(tables))
+
+df0=pd.DataFrame(df[1])
+data22 = df0.drop(["Polling firm","Votes","Lead", "Undecided", "BM 365","Centar","RF", "NS R", "HNS", "KH","IDS","HSS","HSU","HS", "Others"], axis=1)
+headers = ['Date', 'HDZ', 'SDP', 'DP', 'Most', 'Možemo', 'Fokus', 'SD']
+parties = ['HDZ', 'SDP', 'DP', 'Most', 'Možemo', 'Fokus', 'SD']
+data22.columns = headers
+
+data22 = data22[data22['HDZ'] != data22['Fokus']]
+data22['Date'] = data22.Date.apply(lambda x: dateparser.parse(x))
+
+for z in parties:
+  data22[z] = [x.replace('[a]','') for x in data22[z]]
+  data22[z] = [x.replace('[b]','') for x in data22[z]]
+  data22[z] = [x.replace('[c]','') for x in data22[z]]
+  data22[z] = [x.replace('[d]','') for x in data22[z]]
+  data22[z] = [x.replace('[e]','') for x in data22[z]]
+  data22[z] = [x.replace('[f]','') for x in data22[z]]
+  data22[z] = [x.replace('[g]','') for x in data22[z]]
+  data22[z] = [x.replace('[h]','') for x in data22[z]]
+  data22[z] = [x.replace('[i]','') for x in data22[z]]
+  data22[z] = [x.replace('[j]','') for x in data22[z]]
+  data22[z] = [x.replace('[k]','') for x in data22[z]]
+  data22[z] = [x.replace('[l]','') for x in data22[z]]
+  data22[z] = [x.replace('[m]','') for x in data22[z]]
+
+
+print(data22)
+data22.to_csv('Croatia_Elections/poll.csv', index=False)
---
+++ Croatia_Elections/poll.R
@@ -0,0 +1,42 @@
+library(reticulate)
+library(ggplot2)
+library(bbplot)
+library(tidyquant)
+library(scales)
+library(Cairo)
+library(reshape2)
+library(readr)
+library(formattable)
+
+py_run_file("Croatia_Elections/data.py")
+poll <- read_csv("Croatia_Elections/poll.csv")
+d <- melt(poll, id.vars="Date")
+d$value<-as.numeric(d$value)/100
+# d$value[is.nan(d$value)] <- 0
+d$value<-formattable::percent(d$value)
+election<-as.Date("22 07 2024", "%d %m %Y")
+old<-min(d$Date)
+g<-formattable::percent(0.05)
+
+# LOESS GRAPH
+
+plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old,],alpha=0.75)+
+  scale_color_manual(values = c("#255AAA","#DF262D","#C0C0C0",
+                                         "#DC5A2D","#CBE264","#326BA4",
+                                         "#CC272C"))+
+                                           geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.35,linewidth=0.75, data=d[d$Date!=old,])+
+  theme(axis.title=element_blank(),
+        legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.text = element_text(size=16))+
+  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
+  geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  xlim(min(d$Date), election)+
+  geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)+
+  geom_hline(aes(yintercept=g), alpha=0.75, linetype="dashed", colour="#000000")+
+  geom_text(aes(election,g,label = "5% Electoral Threshold", vjust = -1, hjust=1),colour="#56595c")
+
+ggsave(plot=plot, file="Croatia_Elections/plot.png",width = 15, height = 7.5, type = "cairo-png")
