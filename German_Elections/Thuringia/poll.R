e4c393c48c7870b4504493ab5867a48e3018036f
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-01-08 01:48:27 +0000

    new format

--- German_Elections/Thuringia/poll.R
+++ German_Elections/Thuringia/poll.R
@@ -16,52 +16,21 @@ d$value<-as.numeric(d$value)/100
 d$value[is.nan(d$value)] <- 0
 d$value<-formattable::percent(d$value)
 h <- formattable::percent(0.05)
-old <-as.Date("27 10 2019", "%d %m %Y")
+old <-min(d$Date)
 # MAIN GRAPH
 
-plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(data=d[d$Date!=old,],size=0.5) +
+plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old,],alpha=0.75)+
   scale_color_manual(values = c("#BE3075","#009EE0","#000000","#E3000F","#46962b", "#ffed00", "#A2A9B1"))+
-  bbplot::bbc_style()+
-  geom_ma(ma_fun=EMA, n = 3,linetype="solid",linewidth=0.75,wilder=TRUE)+
-  geom_hline(aes(yintercept=h), alpha=0.75)+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.35,linewidth=0.75, data=d[d$Date!=old,])+
+  theme(axis.title=element_blank(),
+        legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.text = element_text(size=16))+
   scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
-  geom_text(aes((min(d$Date)+20),h,label = "5% Electoral Threshold", vjust = -1),colour="#56595c")+
-  geom_point(data=d[d$Date==as.Date("27 10 2019", "%d %m %Y"),],size=4, shape=18, alpha=0.5)
+  xlim(old, max(d$Date))+
+  geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
 
-ggsave(plot=plot1, file="German_Elections/Thuringia/plot1.png",width = 15, height = 7.5, type = "cairo-png")
-
-# LOESS GRAPH
-
-plot2<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(data=d[d$Date!=old,],size=1,alpha=0.75) +
-  scale_color_manual(values = c("#BE3075","#009EE0","#000000","#E3000F","#46962b", "#ffed00", "#A2A9B1"))+
-  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.3,linewidth=0.75,data=d[d$Date!=old,])+
-  # bbplot::bbc_style()+
-  theme(axis.title=element_blank(),legend.title = element_blank(),
-        legend.key.size = unit(2, 'lines'), legend.text = element_text(size=16))+
-  geom_hline(aes(yintercept=h), alpha=0.75)+
-  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
-  geom_text(aes((min(d$Date)+20),h,label = "5% Electoral Threshold", vjust = -1),colour="#56595c")+
-  geom_point(data=d[d$Date==old,],size=4, shape=18, alpha=0.5)
-
-
-ggsave(plot=plot2, file="German_Elections/Thuringia/plot2.png",width = 15, height = 7.5, type = "cairo-png")
-
-# EXPERIMENTAL THINGS
-
-plot3<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(size=0.5) +
-  scale_color_manual(values = c("#BE3075","#009EE0","#000000","#E3000F","#46962b", "#ffed00", "#A2A9B1"))+
-  bbplot::bbc_style()+
-  geom_ma(ma_fun=EMA, n = 5,linetype="solid",size=0.75,ratio=0.1)+
-  theme(axis.title=element_blank(),legend.title = element_blank(),
-        legend.key.size = unit(2, 'lines'), legend.text = element_text(size=16))+
-  geom_hline(aes(yintercept=h), alpha=0.75)+
-  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
-  geom_text(aes((min(d$Date)+20),h,label = "5% Electoral Threshold", vjust = -1),colour="#56595c")+
-  geom_point(data=d[d$Date==as.Date("27 10 2019", "%d %m %Y"),],size=4, shape=18, alpha=0.5)
-
-
-ggsave(plot=plot3, file="German_Elections/Thuringia/plot3.png",width = 15, height = 7.5, type = "cairo-png")
-
+ggsave(plot=plot, file="German_Elections/Thuringia/plot.png",width = 15, height = 7.5, type = "cairo-png")
