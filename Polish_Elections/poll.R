c13a92a09dcbd0a12a878b0a0845bd56c8eb047c
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-01-08 01:33:26 +0000

    New format

--- Polish_Elections/poll.R
+++ Polish_Elections/poll.R
@@ -15,44 +15,24 @@ d$Date<-as.Date(d$Date, "%d %b %Y")
 d$value[is.nan(d$value)] <- 0
 d<-na.omit(d)
 h <- 231
+election<-as.Date("11 11 2023", "%d %m %Y")
+old<-min(d$Date)
 
 # MAIN GRAPH
 
-plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(size=0.5) +
+plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old,],alpha=0.75)+
   scale_color_manual(values = c("#263778","#F68F2D","#851A64","#1BB100","#122746", "#F9C013", "#A2A9B1"))+
-  bbplot::bbc_style()+
-  geom_ma(ma_fun=EMA, n = 3,linetype="solid",linewidth=0.75,wilder=TRUE)+
-  geom_hline(aes(yintercept=h))+
-  geom_text(aes((min(d$Date)+10),h,label = "Majority (231 Seats)", vjust = -1),colour="#56595c")
-
-
-ggsave(plot=plot1, file="Polish_Elections/plot1.png",width = 15, height = 7.5, type = "cairo-png")
-
-# LOESS GRAPH
-
-plot2<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(size=0.5,alpha=0.5) +
-  scale_color_manual(values = c("#263778","#F68F2D","#851A64","#1BB100","#122746", "#F9C013", "#A2A9B1"))+
-  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.3,linewidth=0.75)+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.3,linewidth=0.75, data=d[d$Date!=old,])+
   # bbplot::bbc_style()+
   theme(axis.title=element_blank(),legend.title = element_blank())+
   geom_hline(aes(yintercept=h))+
-  geom_text(aes((min(d$Date)+10),h,label = "Majority (231 Seats)", vjust = -1),colour="#56595c")
+  geom_text(aes((election-5),h,label = "Majority (231 Seats)",hjust=1 ,vjust = -1),colour="#56595c")+
+  xlim(old, election)+
+  geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
 
-ggsave(plot=plot2, file="Polish_Elections/plot2.png",width = 15, height = 7.5, type = "cairo-png")
-
-# EXPERIMENTAL THINGS
-
-plot3<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(size=0.5) +
-  scale_color_manual(values = c("#263778","#F68F2D","#851A64","#1BB100","#122746", "#F9C013", "#A2A9B1"))+
-  bbplot::bbc_style()+
-  geom_ma(ma_fun=EMA, n = 5,linetype="solid",size=0.75,ratio=0.1)+
-  theme(axis.title=element_blank(),legend.title = element_blank())+
-  geom_hline(aes(yintercept=h))+
-  geom_text(aes((min(d$Date)+10),h,label = "Majority (231 Seats)", vjust = -1),colour="#56595c")
-
-
-ggsave(plot=plot3, file="Polish_Elections/plot3.png",width = 15, height = 7.5, type = "cairo-png")
+ggsave(plot=plot, file="Polish_Elections/plot.png",width = 15, height = 7.5, type = "cairo-png")
 
