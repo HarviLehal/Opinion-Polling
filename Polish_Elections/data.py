39a1dc183326c6161bc92cf2edd1a04c67c625e5
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-01-09 18:58:45 +0000

    Corrected

--- Polish_Elections/data.py
+++ Polish_Elections/data.py
@@ -15,27 +15,12 @@ data22 = df7.drop(["Polling Firm/Link", "Sample Size", "Majority", "Kukiz'15"], 
 headers = ['Date', 'United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
 parties = ['United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
 data22.columns = headers
-data22['Date'] = [x.strip()[-11:] for x in data22['Date']]
-data22['Date'] = [x.replace('–','') for x in data22['Date']]
-for z in parties:
-        data22[z] = [x.replace('[av]','') for x in data22[z]]
-        data22[z] = [x.replace('[aw]','') for x in data22[z]]
-        data22[z] = [x.replace('[t]','') for x in data22[z]]
-        data22[z] = [x.replace('[ax]','') for x in data22[z]]
-
 data22.drop(data22.tail(1).index,inplace=True)
 print(data22)
 
 df8=pd.DataFrame(df[9])
 data21 = df8.drop(["Polling Firm/Link", "Sample Size", "Majority"], axis=1)
-headers = ['Date', 'United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others',]
-parties = ['United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
 data21.columns = headers
-data21['United Right'] = [x.replace('[t]','') for x in data21['United Right'].astype(str)]
-for z in parties:
-        data21[z] = data21[z].astype('object').astype(str)
-data21['Date'] = [x.strip()[-11:] for x in data21['Date'].astype(str)]
-data21['Date'] = [x.replace('–','') for x in data21['Date']]
 data21.drop(data21.index[[-1,-3]],inplace=True)
 print(data21)
 
@@ -43,5 +28,16 @@ print(data21)
 
 
 data = pd.concat([data22,data21])
+
+for z in parties:
+        data[z] = [x.replace('[av]','') for x in data[z].astype(str)]
+        data[z] = [x.replace('[aw]','') for x in data[z].astype(str)]
+        data[z] = [x.replace('[t]','') for x in data[z].astype(str)]
+        data[z] = [x.replace('[ax]','') for x in data[z].astype(str)]
+        data[z] = [x.replace('[ay]','') for x in data[z].astype(str)]
+        data[z] = [x.replace('[u]','') for x in data[z].astype(str)]
+        data[z] = data[z].astype('float').astype(str)
+data['Date'] = [x.strip()[-11:] for x in data['Date'].astype(str)]
+data['Date'] = [x.replace('–','') for x in data['Date']]
 print(data)
 data.to_csv('Polish_Elections/poll.csv', index=False)
