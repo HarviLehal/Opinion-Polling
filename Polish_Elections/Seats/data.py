7a9e5c504b5234712c7274cd966f37dfc7251d5b
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-02-02 19:27:46 +0000

    new projections

--- Polish_Elections/Seats/data.py
+++ Polish_Elections/Seats/data.py
@@ -36,6 +36,7 @@ for z in parties:
         data[z] = [x.replace('[ax]','') for x in data[z].astype(str)]
         data[z] = [x.replace('[ay]','') for x in data[z].astype(str)]
         data[z] = [x.replace('[u]','') for x in data[z].astype(str)]
+        data[z] = [x.replace('[v]','') for x in data[z].astype(str)]
         data[z] = data[z].astype('float').astype(str)
 data['Date'] = [x.strip()[-11:] for x in data['Date'].astype(str)]
 data['Date'] = [x.replace('–','') for x in data['Date']]
