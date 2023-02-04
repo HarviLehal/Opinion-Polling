fdd21a30577fd83c3ca524d86dbc409240233686
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-02-04 18:10:59 +0000

    Federal German Election added

---
+++ German_Elections/Federal/data.py
@@ -0,0 +1,34 @@
+import pandas as pd # library for data analysis
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+import numpy as np
+import dateparser
+
+wikiurl="https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_German_federal_election"
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table',class_="wikitable")
+df=pd.read_html(str(tables))
+
+headers = ['Date','SPD','Union','Grüne','FDP','AfD','Linke']
+parties = ['SPD','Union','Grüne','FDP','AfD','Linke']
+d = {}
+for i in range(3):
+  d[i]=pd.DataFrame(df[i])
+  d[i]=d[i].drop(["Polling firm", "Sample size", "Abs.", "Others", "Lead"], axis=1)
+  d[i].columns = headers
+  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
+  d[i].Date2.fillna(d[i].Date, inplace=True)
+  # d[i]['Date2'] = [x+ str(2023-i) for x in d[i]['Date2'].astype(str)]
+  d[i]['Date'] = d[i]['Date2']
+  d[i] = d[i].drop(['Date2'], axis=1)
+  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x))
+  d[i] = d[i][d[i]['SPD'] != d[i]['Linke']]
+
+for i in range(2):
+  d[i].drop(d[i].index[[-1]],inplace=True)
+
+D = pd.concat(d.values(), ignore_index=True)
+D.to_csv('German_Elections/Federal/poll.csv', index=False)
