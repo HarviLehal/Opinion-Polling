1c55315473c2cadd609fd2c4f030b36136e0439b
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-02-04 23:08:56 +0000

    new format

--- Lithuanian_Elections/poll.R
+++ Lithuanian_Elections/poll.R
@@ -7,6 +7,7 @@ library(Cairo)
 library(reshape2)
 library(readr)
 library(formattable)
+library(ggpubr)
 
 py_run_file("Lithuanian_Elections/data.py")
 poll <- read_csv("Lithuanian_Elections/poll.csv")
@@ -44,3 +45,56 @@ plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   
 ggsave(plot=plot, file="Lithuanian_Elections/plot.png",width = 15, height = 7.5, type = "cairo-png")
  
+
+
+# BAR CHART!!
+poll <- read_csv("Lithuanian_Elections/poll.csv")
+# poll<-subset(poll,select=-c(Other))
+poll$Date <- as.Date(poll$Date, "%d %b %Y")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(sub("%","",as.character(x)))))
+d2 <- poll[poll$Date==min(poll$Date),]
+poll<-poll[poll$Date>(max(poll$Date)-30),]
+poll[-1][is.na(poll[-1])] <- 0
+d2[-1][is.na(d2[-1])] <- 0
+d1 <- colMeans(poll[-1])
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+d2 <- as.data.frame(d2)
+
+d1 <- melt(d1, id.vars="Date")
+d1$value<-as.numeric(d1$value)/100
+d1$value<-formattable::percent(d1$value, digits = 1)
+
+d2 <- melt(d2, id.vars="Date")
+d2$value<-as.numeric(d2$value)/100
+d2$value<-formattable::percent(d2$value, digits = 1)
+
+d3<-rbind(d2,d1)
+
+plot4<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+  geom_bar(stat="identity",width=0.9, position=position_dodge())+
+  scale_fill_manual(values = c("#90d1cb","#3DA49A","#85c785","#319032","#83a0c7","#2D568C","#eb8188","#D41720",
+                               "#eb81b2","#D6136E","#f5bf84","#E98313","#b86e7c","#711625","#e08d90","#C2312F",
+                               "#8ccf8f","#369C3A","#fadc84","#F3BB0C","#6c6aba","#121072"))+
+  geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value,""), digits = 1),y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d3$value,")"),""),y = 0),
+            hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background= element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill="#FFFFFF",color="#FFFFFF"))+
+  ggtitle('30 day average \n (2020 Result)')+
+  scale_x_discrete(limits = rev(levels(d3$variable)))+
+  coord_flip()
+
+plot<-plot+theme(legend.position = "none")
+plot2<-ggarrange(plot, plot4,ncol = 2, nrow = 1,widths=c(2,0.5))
+
+ggsave(plot=plot2, file="Lithuanian_Elections/plot2.png",width = 15, height = 7.5, type = "cairo-png")
