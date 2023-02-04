15f7c0fb44ac974d89b624ca276d58deaab49967
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-02-04 20:55:32 +0000

    New polls added

--- UK_Elections/general_polling/poll.R
+++ UK_Elections/general_polling/poll.R
@@ -143,7 +143,7 @@ plot4<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), 
   theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
-        plot.background = element_rect(fill = "#FFFFFF"))+
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
   ggtitle('30 day average \n (2019 Result)')+
   scale_x_discrete(limits = rev(levels(d3$variable)))+
   coord_flip()
