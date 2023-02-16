cfb157441632c1d53ac83bc3a8105987d7724652
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-02-16 20:47:59 +0000

    Australian Election added

---
+++ Australia_Elections/data.py
@@ -0,0 +1,43 @@
+import pandas as pd # library for data analysis
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+import numpy as np
+import dateparser
+
+wikiurl="https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Australian_federal_election"
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table',class_="wikitable")
+df=pd.read_html(str(tables))
+
+
+headers=['Date', 'Brand', 'mode', 'Sample', 'Coalition', 'Labor', 'Green', 'ONP', 'UAP', 'Other', 'UND', 'Labor2', 'Coalition2']
+parties = ['Coalition', 'Labor', 'Green', 'ONP', 'UAP', 'Other', 'Labor2', 'Coalition2']
+d = {}
+for i in range(2):
+  d[i]=pd.DataFrame(df[i])
+  d[i].columns = headers
+  d[i]=d[i].drop(["Brand", "Sample", "UND", "mode"], axis=1)
+  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
+  d[i].Date2.fillna(d[i].Date, inplace=True)
+  # d[i]['Date2'] = [x+ str(2023-i) for x in d[i]['Date2'].astype(str)]
+  d[i]['Date'] = d[i]['Date2']
+  d[i] = d[i].drop(['Date2'], axis=1)
+  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x))
+  d[i] = d[i][d[i]['Labor'] != d[i]['Other']]
+  for z in parties:
+    d[i][z] = [x.replace('[c]','') for x in d[i][z].astype(str)]
+
+D = pd.concat(d.values(), ignore_index=True)
+PP = ['Labor2', 'Coalition2']
+E = D.drop(PP, axis=1)
+E.to_csv('Australia_Elections/poll.csv', index=False)
+
+
+parties2 = ['Coalition', 'Labor', 'Green', 'ONP', 'UAP', 'Other']
+D = D.drop(parties2, axis=1)
+headers = ['Date', 'Labor', 'Coalition']
+D.columns = headers
+D.to_csv('Australia_Elections/poll2.csv', index=False)
