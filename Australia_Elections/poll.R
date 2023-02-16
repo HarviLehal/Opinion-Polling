fd38953b5df60ff7408cbee91e4204c8f3bd4667
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-02-16 20:48:30 +0000

    Fixed graphs

--- Australia_Elections/poll.R
+++ Australia_Elections/poll.R
@@ -14,14 +14,13 @@ library(zoo)
 library(tidyverse)
 library(data.table)
 library(hrbrthemes)
-py_run_file("NewZealand_Elections/data.py")
-poll <- read_csv("NewZealand_Elections/poll.csv")
+py_run_file("Australia_Elections/data.py")
+poll <- read_csv("Australia_Elections/poll.csv")
 d <- melt(poll, id.vars="Date")
-d$value<-as.numeric(d$value)/100
+d$value<-as.numeric(sub("%","",d$value))/100
 d$value<-formattable::percent(d$value)
-h <- formattable::percent(0.05)
 
-election<-as.Date("14 10 2023", "%d %m %Y")
+election<-as.Date("27 09 2025", "%d %m %Y")
 old <-min(d$Date)
 # MAIN GRAPH
 
@@ -29,16 +28,15 @@ old <-min(d$Date)
 
 plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_point(size=1, data=d[d$Date!=old,],alpha=0.5)+
-  scale_color_manual(values = c("#CC3028","#00529F",
-                                "#098137","#FBBE00"))+
-  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.25,linewidth=0.75, data=d[d$Date!=old,])+
+  scale_color_manual(values = c("#2031CC","#D23A38",
+                                "#3AA54F","#E76E29",
+                                "#F8CC10","#a2aab3"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.7,linewidth=0.75, data=d[d$Date!=old,])+
   # bbplot::bbc_style()+
   theme(axis.title=element_blank(),legend.title = element_blank(),
         legend.key.size = unit(2, 'lines'),
         legend.position = "none")+
   scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
-  geom_hline(aes(yintercept=h), alpha=0.75, linetype="longdash", colour="#000000")+
-  geom_text(aes(election,h,label = "5% Party Threshold", vjust = -1, hjust=1),colour="#56595c")+
   geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   xlim(min(d$Date), election)+
   geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
@@ -46,14 +44,14 @@ plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
 
 
-poll <- read_csv("NewZealand_Elections/poll.csv")
+poll <- read_csv("Australia_Elections/poll.csv")
 # poll$Date <- as.Date(poll$Date, "%d %b %Y")
 Date <- c(max(poll$Date))
 poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
   as.numeric(sub("%","",as.character(x)))))
 d2 <- poll[poll$Date==min(poll$Date),]
 poll<-poll[poll$Date>(max(poll$Date)-30),]
-d1 <- colMeans(poll[-1])
+d1 <- colMeans(poll[-1],na.rm = TRUE)
 d1 <- as.data.frame(d1)
 d1 <- t(d1)
 d1 <- cbind(Date, d1)
@@ -62,11 +60,11 @@ d1$Date <- as.Date(d1$Date)
 d2 <- as.data.frame(d2)
 
 d1 <- melt(d1, id.vars="Date")
-d1$value<-as.numeric(d1$value)/100
+d1$value<-as.numeric(sub("%","",d1$value))/100
 d1$value<-formattable::percent(d1$value, digits = 1)
 
 d2 <- melt(d2, id.vars="Date")
-d2$value<-as.numeric(d2$value)/100
+d2$value<-as.numeric(sub("%","",d2$value))/100
 d2$value<-formattable::percent(d2$value, digits = 1)
 
 d3<-rbind(d2,d1)
@@ -76,8 +74,9 @@ d3<-rbind(d2,d1)
 
 plot2<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
 geom_bar(stat="identity",width=0.9, position=position_dodge())+
-scale_fill_manual(values = c("#F7B7B7","#CC3028","#B5CEFF","#00529F",
-                             "#9EED8E","#098137","#fadc7d","#FBBE00"))+
+scale_fill_manual(values = c("#B5CEFF","#2031CC","#F7B7B7","#D23A38",
+                             "#9EED8E","#3AA54F","#f7b792","#E76E29",
+                             "#fadc7d","#F8CC10","#d0d4d9","#a2aab3"))+
 geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value, ""), digits = 1),
               y = 0),
           hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
@@ -97,18 +96,16 @@ coord_flip()
 plot<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.5))
 plot
 
-ggsave(plot=plot, file="NewZealand_Elections/plot.png",width = 15, height = 7.5, type="cairo-png")
+ggsave(plot=plot, file="Australia_Elections/plot.png",width = 15, height = 7.5, type="cairo-png")
 
 
 # COALITION
 
-poll <- read_csv("NewZealand_Elections/poll2.csv")
+poll <- read_csv("Australia_Elections/poll2.csv")
 d <- melt(poll, id.vars="Date")
-d$value<-as.numeric(d$value)/100
-d$value<-formattable::percent(d$value)
+d$value<-as.numeric(sub("%","",d$value))/100
 h <- formattable::percent(0.05)
 
-election<-as.Date("14 10 2023", "%d %m %Y")
 old <-min(d$Date)
 # MAIN GRAPH
 
@@ -116,22 +113,20 @@ old <-min(d$Date)
 
 plot1a<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_point(size=1, data=d[d$Date!=old,],alpha=0.5)+
-  scale_color_manual(values = c("#CC3028","#00529F"))+
-  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.25,linewidth=0.75, data=d[d$Date!=old,])+
+  scale_color_manual(values = c("#D23A38","#2031CC"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.7,linewidth=0.75, data=d[d$Date!=old,])+
   # bbplot::bbc_style()+
   theme(axis.title=element_blank(),legend.title = element_blank(),
         legend.key.size = unit(2, 'lines'),
         legend.position = "none")+
   scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
-  geom_hline(aes(yintercept=h), alpha=0.75, linetype="longdash", colour="#000000")+
-  geom_text(aes(election,h,label = "5% Party Threshold", vjust = -1, hjust=1),colour="#56595c")+
   geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   xlim(min(d$Date), election)+
   geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
   geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
 
-poll <- read_csv("NewZealand_Elections/poll2.csv")
+poll <- read_csv("Australia_Elections/poll2.csv")
 # poll$Date <- as.Date(poll$Date, "%d %b %Y")
 Date <- c(max(poll$Date))
 poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
@@ -158,10 +153,9 @@ d3<-rbind(d2,d1)
 
 
 
-
 plot2a<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
   geom_bar(stat="identity",width=0.9, position=position_dodge())+
-  scale_fill_manual(values = c("#F7B7B7","#CC3028","#B5CEFF","#00529F"))+
+  scale_fill_manual(values = c("#F7B7B7","#D23A38","#B5CEFF","#2031CC"))+
   geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value, ""), digits = 1),y = 0),
             hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
   geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),y = 0),
@@ -177,7 +171,7 @@ plot2a<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable),
 
 
 plot2<-ggarrange(plot1a, plot2a,ncol = 2, nrow = 1,widths=c(2,0.5))
-plot
+plot2
 
-ggsave(plot=plot2, file="NewZealand_Elections/plot2.png",width = 15, height = 7.5, type="cairo-png")
+ggsave(plot=plot2, file="Australia_Elections/plot2.png",width = 15, height = 7.5, type="cairo-png")
 
