3b2b406687d4fd7c1ff432e6b03d60dd8e1b1cac
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-03-15 22:59:47 +0000

    Campaign period added

---
+++ Turkish/Campaign/data.py
@@ -0,0 +1,33 @@
+import pandas as pd # library for data analysis
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+import dateparser
+
+wikiurl="https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Turkish_presidential_election"
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table',class_="wikitable")
+df=pd.read_html(str(tables))
+
+# 2023 POLLS
+
+df0=pd.DataFrame(df[0])
+data = df0.drop(['Polling firm','Published by','Sample size', 'Others','Lead'], axis=1)
+headers = ['Date', 'Erdoğan', 'Kılıçdaroğlu', 'Oğan', 'İnce']
+parties = ['Erdoğan', 'Kılıçdaroğlu', 'Oğan', 'İnce']
+data.columns = headers
+data = data[data['Erdoğan'] != data['İnce']]
+data['Date2'] = data['Date'].str.split('–').str[1]
+data.Date2.fillna(data.Date, inplace=True)
+data['Date2'] = [x+' 2023' for x in data['Date2'].astype(str)]
+data['Date'] = data['Date2']
+data = data.drop(['Date2'], axis=1)
+
+print(data)
+
+
+data.Date = data.Date.astype(str).apply(lambda x: dateparser.parse(x))
+print(data)
+data.to_csv('Turkish/Campaign/poll.csv', index=False)
---
+++ Turkish/Campaign/poll.R
@@ -0,0 +1,45 @@
+library(reticulate)
+library(ggplot2)
+library(bbplot)
+library(tidyquant)
+library(scales)
+library(Cairo)
+library(reshape2)
+library(readr)
+library(formattable)
+
+py_run_file("Turkish/Campaign/data.py")
+poll <- read_csv("Turkish/Campaign/poll.csv")
+
+
+d <- reshape2::melt(poll, id.vars="Date")
+d$value<-as.numeric(d$value)/100
+d$value<-formattable::percent(d$value)
+election<-as.Date("14 05 2023", "%d %m %Y")
+d$Date<-as.Date(d$Date)
+old<-as.Date("10 03 2023", "%d %m %Y")
+h <- formattable::percent(0.5)
+
+# MAIN GRAPH
+
+plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old,],alpha=0.75)+
+  scale_color_manual(values = c("#FFCC00","#ed1c24","#3e4042","#0d5ca6"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.4,linewidth=0.75, data=d[d$Date!=old,])+
+  theme(axis.title=element_blank(),
+        legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.text = element_text(size=16))+
+  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
+  geom_hline(aes(yintercept=h))+
+  geom_text(aes((election),h,label = "50%",hjust=1 ,vjust = -1),colour="#56595c")+
+  geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  xlim(old, election)+
+  geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
+
+ggsave(plot=plot, file="Turkish/Campaign/plot.svg",width = 15, height = 7.5)
+aaa=readLines("Turkish/Campaign/plot.svg",-1)
+bbb <- gsub(".svglite ", "", aaa)
+writeLines(bbb,"Turkish/Campaign/plot.svg")
