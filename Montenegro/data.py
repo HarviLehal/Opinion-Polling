79cec8636a1724f9c76f6b16ad58c74b94022c5a
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-04-21 00:44:51 +0100

    Montenegro Added

---
+++ Montenegro/data.py
@@ -0,0 +1,38 @@
+import pandas as pd # library for data analysis
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+import numpy as np
+import dateparser
+import re
+
+
+wikiurl="https://en.wikipedia.org/wiki/2023_Montenegrin_parliamentary_election"
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table',class_="wikitable")
+df=pd.read_html(str(tables))
+p = re.compile(r'\[[a-z]+\]')
+
+data22=pd.DataFrame(df[1])
+data22 = data22.drop(['Polling firm/source','Others','Lead','SNP','UCG','Prava','DP','Demos','LSh','KSh'],axis=1)
+headers = ['Date','DPS','SD','DF','DCG','CnB','BS','SDP','PES']
+parties = ['DPS','SD','DF','DCG','CnB','BS','SDP','PES']
+data22.columns = headers
+
+
+data22['Date2'] = data22['Date'].str.split('–').str[1]
+data22.Date2.fillna(data22['Date'].str.split('-').str[1], inplace=True)
+data22.Date2.fillna(data22.Date, inplace=True)
+data22.Date = data22.Date2
+data22 = data22.drop(['Date2'],axis=1)
+data22.Date = data22['Date'].astype(str)
+data22.Date = data22.Date.apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
+
+for z in parties:
+  data22[z] = [p.sub('', x) for x in data22[z].astype(str)]
+  data22[z] = [x.replace('–',str(np.NaN)) for x in data22[z]]
+  data22[z] = data22[z].astype('float').astype(str)
+
+data22.to_csv('Montenegro/poll.csv', index=False)
---
+++ Montenegro/poll.R
@@ -0,0 +1,45 @@
+library(reticulate)
+library(ggplot2)
+library(bbplot)
+library(tidyquant)
+library(scales)
+library(Cairo)
+library(reshape2)
+library(readr)
+library(formattable)
+library(svglite)
+library(Rcpp)
+library(ggpubr)
+
+py_run_file("Montenegro/data.py")
+poll <- read_csv("Montenegro/poll.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+d$value<-as.numeric(d$value)/100
+d$value<-formattable::percent(d$value)
+h <- formattable::percent(0.03)
+
+election<-as.Date("11 06 2023", "%d %m %Y")
+old <-min(d$Date)
+
+# LOESS GRAPH
+
+plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old,],alpha=0.35)+
+  scale_color_manual(values = c("#6866B2","#F08080","#00008B","#F5911D","#800080","#32CD32","#FF0000","#F8D810"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.6,linewidth=0.75, data=d[d$Date!=old,])+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'))+
+  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
+  geom_hline(aes(yintercept=h), alpha=0.75, linetype="longdash", colour="#000000")+
+  geom_text(aes(election,h,label = "3% Party Threshold", vjust = -1, hjust=1),colour="#56595c")+
+  geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  xlim(min(d$Date), election)+
+  geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
+
+plot
+ggsave(plot=plot, file="Montenegro/plot.svg",width = 15, height = 7.5)
+aaa=readLines("Montenegro/plot.svg",-1)
+bbb <- gsub(".svglite ", "", aaa)
+writeLines(bbb,"Montenegro/plot.svg")
