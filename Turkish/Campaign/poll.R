b8809a68fb8eb201e1d95b3b514fac271596b548
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-05-11 01:03:01 +0100

    New Polls 10-V-2023

--- Turkish/Campaign/poll.R
+++ Turkish/Campaign/poll.R
@@ -18,7 +18,9 @@ d$value<-formattable::percent(d$value)
 election<-as.Date("14 05 2023", "%d %m %Y")
 d$Date<-as.Date(d$Date)
 old<-as.Date("10 03 2023", "%d %m %Y")
+ban<-as.Date("04 05 2023", "%d %m %Y")
 h <- formattable::percent(0.5)
+g <- formattable::percent(0.54)
 
 # MAIN GRAPH
 
@@ -26,7 +28,7 @@ plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_point(size=1, data=d,alpha=0.75)+
   scale_color_manual(values = c("#FFCC00","#ed1c24",
                                 "#3e4042","#0d5ca6"))+
-  geom_smooth(method='loess',fullrange=TRUE,span=0.6,se=FALSE,linewidth=0.75, data=d)+
+  geom_smooth(method='loess',fullrange=TRUE,span=0.5,se=FALSE,linewidth=0.75, data=d)+
   # stat_smooth(method='lm', formula = y~poly(x,2),se=FALSE)+
   theme(axis.title=element_blank(),
         legend.title = element_blank(),
@@ -36,6 +38,8 @@ plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_hline(aes(yintercept=h))+
   geom_text(aes((election),h,label = "50%",hjust=1 ,vjust = -1),colour="#56595c")+
   geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=ban, linetype="dashed", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_text(aes(ban,g,label = "Polling Ban", vjust = -1, angle=-90))+
   xlim(old, election)+
   geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)
 plot
