ce3a7c38f96ab9dc37e40297ea5e05598a04ac12
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-05-19 20:27:40 +0100

    Results Added

--- German/State/Bremen/poll.R
+++ German/State/Bremen/poll.R
@@ -22,7 +22,7 @@ old<-min(d$Date)
 # MAIN GRAPH
 
 plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(size=1,alpha=0.5) +
+  geom_point(size=1,alpha=0.5,data=d[d$Date!=old|d$Date!=election,]) +
   scale_color_manual(values = c("#000000","#E3000F","#46962b","#BE3075","#009EE0", "#ffed00", "#0000AA", "#A2A9B1"))+
   geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.75,linewidth=0.75, data=d[d$Date!=old,])+
   theme(axis.title=element_blank(),
@@ -33,10 +33,10 @@ plot<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   xlim(old,election)+
   geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
-  geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
-  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)+
   scale_x_break(c(old+90, as.Date("01 01 2022", "%d %m %Y")))+
   scale_x_date(date_breaks = "3 month", date_labels =  "%m %Y",limits = c(old-10,election+10))
-
+plot
 ggsave(plot=plot, file="German/State/Bremen/plot.png",width = 15, height = 7.5, type = "cairo-png")
 
