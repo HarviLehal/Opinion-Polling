fbdb2f6196c7081eaf3b914ec56d446e90cb00a4
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-05-28 19:31:12 +0100

    new swedish graphs added

---
+++ Swedish/poll_old.R
@@ -0,0 +1,94 @@
+library(reticulate)
+library(ggplot2)
+library(bbplot)
+library(tidyquant)
+library(scales)
+library(Cairo)
+library(reshape2)
+library(readr)
+library(formattable)
+library(svglite)
+library(Rcpp)
+library(ggpubr)
+library(zoo)
+library(tidyverse)
+library(data.table)
+
+py_run_file("Swedish/data_old.py")
+poll <- read_csv("Swedish/poll_old.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+d$value<-as.numeric(d$value)/100
+d$value<-formattable::percent(d$value)
+h<-formattable::percent(0.04)
+election<-as.Date("11 09 2022", "%d %m %Y")
+old <-min(d$Date)
+
+# MAIN GRAPH
+
+plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old|d$Date!=election,],alpha=0.5)+
+  scale_color_manual(values = c("#A60D0E","#DF253A","#419129","#1E4838", "#2B69B3", "#3F9BDB","#255DA1","#F9E00C"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.4,linewidth=0.75, data=d[d$Date!=old|d$Date!=election,])+
+  # bbplot::bbc_style()+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")+
+  geom_hline(aes(yintercept=h))+
+  geom_text(aes((election-5),h,label = "4% Threshold",hjust=1 ,vjust = -1),colour="#56595c")+
+  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
+  geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  xlim(min(d$Date), election)+
+  geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)
+
+
+poll <- read_csv("Swedish/poll.csv")
+poll$Date <- as.Date(poll$Date, "%d %b %Y")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(sub("%","",as.character(x)))))
+d2 <- poll[poll$Date==min(poll$Date),]
+poll<-poll[poll$Date>(max(poll$Date)-30),]
+d1 <- colMeans(poll[-1])
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+d2 <- as.data.frame(d2)
+
+d1 <- reshape2::melt(d1, id.vars="Date")
+d1$value<-as.numeric(d1$value)/100
+d1$value<-formattable::percent(d1$value, digits = 1)
+
+d2 <- reshape2::melt(d2, id.vars="Date")
+d2$value<-as.numeric(d2$value)/100
+d2$value<-formattable::percent(d2$value, digits = 1)
+
+d3<-rbind(d2,d1)
+
+plot2<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+geom_bar(stat="identity",width=0.9, position=position_dodge())+
+scale_fill_manual(values = c("#d17171","#A60D0E","#f08d97","#DF253A",
+                             "#93c981","#419129","#74a391","#1E4838",
+                             "#86add9","#2B69B3","#9acced","#3F9BDB",
+                             "#7ea2cf","#255DA1","#fcf086","#F9E00C"))+
+geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value, ""), digits = 1),y = 0),
+          hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),y = 0),
+          hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+theme_minimal()+
+theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+      panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+      plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+ggtitle('30 day average \n (2018 Result)')+
+scale_x_discrete(limits = rev(levels(d3$variable)))+
+coord_flip()
+
+
+plotA<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.5))
+plotA
+
+ggsave(plot=plotA, file="Swedish/plot_old.png",width = 15, height = 7.5, type="cairo-png")
