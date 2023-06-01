9d652b2003cd2fc848ee008ebb6ef19f93d56512
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-06-01 16:34:56 +0100

    New Polls 01-VI-2023

--- Polish/Seats/data.py
+++ Polish/Seats/data.py
@@ -3,6 +3,7 @@ import requests # library to handle requests
 from bs4 import BeautifulSoup # library to parse HTML documents
 import dateparser
 import re
+import numpy as np
 
 wikiurl="https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Polish_parliamentary_election"
 table_class="wikitable sortable jquery-tablesorter"
@@ -13,7 +14,7 @@ tables = soup.find_all('table',class_="wikitable")
 df=pd.read_html(str(tables))
 
 df6=pd.DataFrame(df[9])
-data23 = df6.drop(["Polling Firm/Link", "Sample Size", "Majority"], axis=1)
+data23 = df6.drop(["Polling Firm/Link", "Sample Size", "Majority",'KO+L+KP +P2050'], axis=1)
 headers = ['Date','United Right','Civic Coalition','The Left','Polish Coalition','Poland 2050','Confederation','Others']
 parties = ['United Right','Civic Coalition','The Left','Polish Coalition','Poland 2050','Confederation','Others']
 threeway = ['Polish Coalition','Poland 2050']
@@ -24,7 +25,7 @@ data23['Third Way']=np.where(data23['Polish Coalition']<40, data23['Polish Coali
 data23=data23.drop(threeway, axis=1)
 
 df7=pd.DataFrame(df[10])
-data22 = df7.drop(["Polling Firm/Link", "Sample Size", "Majority", "Kukiz'15"], axis=1)
+data22 = df7.drop(["Polling Firm/Link", "Sample Size", "Majority", "Kukiz'15",'KO+L+KP +P2050'], axis=1)
 headers = ['Date', 'United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
 parties = ['United Right', 'Civic Coalition', 'The Left', 'Polish Coalition', 'Confederation', 'Poland 2050', 'Others']
 data22.columns = headers
@@ -32,7 +33,7 @@ data22.drop(data22.tail(1).index,inplace=True)
 print(data22)
 
 df8=pd.DataFrame(df[11])
-data21 = df8.drop(["Polling Firm/Link", "Sample Size", "Majority"], axis=1)
+data21 = df8.drop(["Polling Firm/Link", "Sample Size", "Majority",'KO+L+KP +P2050'], axis=1)
 data21.columns = headers
 data21.drop(data21.index[[-1,-3]],inplace=True)
 print(data21)
