7686057baf9a10b464bbe0f6f976ea1e4a3e4451
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-08-20 20:52:48 +0100

    New Polls 20-VIII-2023

---
+++ Japan/data.py
@@ -0,0 +1,55 @@
+import pandas as pd # library for data analysis
+import requests # library to handle requests
+from bs4 import BeautifulSoup # library to parse HTML documents
+import numpy as np
+import dateparser
+
+wikiurl="https://en.wikipedia.org/wiki/Next_Japanese_general_election"
+table_class="wikitable sortable jquery-tablesorter"
+response=requests.get(wikiurl)
+print(response.status_code)
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table',class_="wikitable")
+df=pd.read_html(str(tables))
+
+headers = ['Date','LDP','CDP','NIK','KMT','JCP','DPP','REI','DIY','SDP','SJ48','None']
+parties = ['LDP','CDP','NIK','KMT','JCP','DPP','REI','DIY','SDP','SJ48','None']
+d = {}
+
+for i in range(1):
+  d[i]=pd.DataFrame(df[2])
+  d[i]=d[i].drop(["Respondents","Polling firm","Others", "Und./ no ans.", "Lead"], axis=1)
+  d[i].columns = headers
+  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
+  d[i].Date2.fillna(d[i].Date, inplace=True)
+  # d[i]['Date2'] = [x+ str(2023-i) for x in d[i]['Date2'].astype(str)]
+  d[i]['Date'] = d[i]['Date2']
+  d[i] = d[i].drop(['Date2'], axis=1)
+  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
+  d[i] = d[i][d[i]['LDP'] != d[i]['KMT']]
+  for z in parties:
+    d[i][z] = d[i][z].astype('string')
+  for z in parties:
+    d[i][z] = [x.replace('–',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('-',str(np.NaN)) for x in d[i][z]]
+    
+  for z in parties:
+    d[i][z] = d[i][z].astype('float')
+
+D = pd.concat(d.values(), ignore_index=True)
+D.to_csv('Japan/poll.csv', index=False)
+
+
+
+D['None'].fillna(0, inplace=True)
+D['total']=D[parties].sum(axis=1)
+
+D['decided']=D['total']-D['None']
+
+print(D)
+parties = ['LDP','CDP','NIK','KMT','JCP','DPP','REI','DIY','SDP','SJ48']
+D[parties] = D[parties].div(D['decided'], axis=0)*100
+
+D = D.drop(["decided","total","None"], axis=1)
+
+D.to_csv('Japan/poll2.csv', index=False)
---
+++ Japan/poll2.R
@@ -0,0 +1,117 @@
+library(reticulate)
+library(ggplot2)
+library(bbplot)
+library(tidyquant)
+library(scales)
+library(Cairo)
+library(reshape2)
+library(readr)
+library(formattable)
+library(svglite)
+library(Rcpp)
+library(ggpubr)
+library(zoo)
+library(tidyverse)
+library(data.table)
+library(hrbrthemes)
+poll <- read_csv("Japan/poll2.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+d$value<-as.numeric(d$value)/100
+d$value<-formattable::percent(d$value)
+
+election<-as.Date("29 10 2025", "%d %m %Y")
+old <-min(d$Date)
+# MAIN GRAPH
+
+# LOESS GRAPH
+
+plot1<-ggplot(data=d[d$Date!=old,],aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old,],alpha=0.5)+
+  scale_color_manual(values = c("#3ca324","#184589","#b8ce43","#f95580","#db001c",
+                                "#ffba00","#ed008c","#ed7301","#1ca9e9","#e289b6"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.6,linewidth=0.75, data=d[d$Date!=old,])+
+  # bbplot::bbc_style()+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")+
+  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
+  geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)
+  # xlim(min(d$Date), election)
+  # geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)
+  # geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
+  # geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
+plot1 
+
+d <- d %>%
+  group_by(variable) %>%
+  arrange(Date) %>%
+  mutate(Moving_Average = rollapply(value, width=7, FUN=function(x) mean(x, na.rm=TRUE), by=1, by.column=TRUE, partial=TRUE, fill=NA, align="right"))
+
+plot1a<-ggplot(data=d[d$Date!=old,],aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=0.5, data=d[d$Date!=old,]) +
+  scale_color_manual(values = c("#3ca324","#184589","#b8ce43","#f95580","#db001c",
+                                "#ffba00","#ed008c","#ed7301","#1ca9e9","#e289b6"))+
+  scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
+  geom_line(aes(y = Moving_Average), linetype = "solid", size=0.75)+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")
+
+  
+poll <- read_csv("Japan/poll2.csv")
+# poll$Date <- as.Date(poll$Date, "%d %b %Y")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(sub("%","",as.character(x)))))
+d2 <- poll[poll$Date==min(poll$Date),]
+poll<-poll[poll$Date>(max(poll$Date)-7),]
+d1 <- colMeans(poll[-1])
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+d2 <- as.data.frame(d2)
+
+d1 <- reshape2::melt(d1, id.vars="Date")
+d1$value<-as.numeric(d1$value)/100
+d1$value<-formattable::percent(d1$value, digits = 1)
+
+d2 <- reshape2::melt(d2, id.vars="Date")
+d2$value<-as.numeric(d2$value)/100
+d2$value<-formattable::percent(d2$value, digits = 1)
+
+d3<-rbind(d2,d1)
+
+
+plot2<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+geom_bar(stat="identity",width=0.9, position=position_dodge())+
+scale_fill_manual(values = c("#77bf66","#3ca324","#748fb8","#184589",
+                             "#d4e28e","#b8ce43","#fb99b3","#f95580",
+                             "#e96677","#db001c","#ffd666","#ffba00",
+                             "#f466ba","#ed008c","#f4ab67","#ed7301",
+                             "#77cbf2","#1ca9e9","#eeb8d3","#e289b6"))+
+geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value, ""), digits = 1),
+              y = 0),
+          hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
+              y = 0),
+          hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+theme_minimal()+
+theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+      panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+      plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+ggtitle('7 day average \n (2021 Result)')+
+coord_flip()
+
+
+plot<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.5))
+plot
+plota<-ggarrange(plot1a, plot2,ncol = 2, nrow = 1,widths=c(2,0.5))
+plota
+
+ggsave(plot=plot, file="Japan/plot2.svg",width = 15, height = 7.5)
+aaa=readLines("Japan/plot2.svg",-1)
+bbb <- gsub(".svglite ", "", aaa)
+writeLines(bbb,"Japan/plot2.svg")
