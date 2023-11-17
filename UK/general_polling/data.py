9961496c3af597327e990ccbf93367d866471373
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-11-17 18:02:56 +0000

    New Polls 17-XI-2023

--- UK/general_polling/data.py
+++ UK/general_polling/data.py
@@ -23,7 +23,8 @@ for i in range(4):
   d[i].columns = headers
   for z in parties:
     d[i][z] = [p.sub('', x) for x in d[i][z].astype(str)]
-    d[i][z] = [x.replace('–',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('-',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('—',str(np.NaN)) for x in d[i][z]]
     d[i][z] = [x.replace('–',str(np.NaN)) for x in d[i][z]]
     d[i][z] = [x.replace('TBC',str(np.NaN)) for x in d[i][z]]
     d[i][z] = [x.replace('TBA',str(np.NaN)) for x in d[i][z]]
