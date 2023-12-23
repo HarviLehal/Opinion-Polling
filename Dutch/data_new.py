98c34ca3b8a186e65da2181ad46e110703af6afc
HarviLehal <sma18hsl@liverpool.ac.uk>
2023-12-23 15:58:20 +0000

    New Polls 23-XII-2023

--- Dutch/data_new.py
+++ Dutch/data_new.py
@@ -17,36 +17,8 @@ p = re.compile(r'\[[a-z]+\]'  )
 data2=pd.DataFrame(df[1])
 data2=data2.drop(["Polling firm", "Sample size", "Lead","Ref"], axis=1)
 
-# headers = ['Date','PVV','PvdA','GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
-# parties = ['PVV','PvdA','GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
-# data2.columns = headers
-# 
-# data2['Date2'] = data2['Date'].str.split('–').str[1]
-# data2.Date2.fillna(data2['Date'].str.split('-').str[1], inplace=True)
-# data2.Date2.fillna(data2.Date, inplace=True)
-# data2.Date = data2.Date2
-# data2 = data2.drop(['Date2'],axis=1)
-# data2.Date = data2['Date'].astype(str)
-# data2.Date = data2.Date.apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
-# data2 = data2[data2['PVV'] != data2['SGP']]
-# for z in parties:
-#   data2[z] = [p.sub('', x) for x in data2[z].astype(str)]
-#   data2[z] = [x.replace('–',str(np.NaN)) for x in data2[z].astype(str)]
-#   data2[z] = [x.replace('–',str(np.NaN)) for x in data2[z].astype(str)]
-# data2[parties] = data2[parties].astype(float)
-# data2['PvdA']=np.where(data2['PvdA']==data2['GL'], 0, data2['PvdA'])
-# Fusie=['PvdA','GL']
-# data2[Fusie] = data2[Fusie].astype(float)
-# data2['PvdA-GL'] = data2[Fusie].sum(axis=1)
-# data2 = data2.drop(Fusie, axis=1)
-# 
-# data2 = data2[['Date','PVV','PvdA-GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']]
-
-
-
-'''CORRECTION'''
-headers = ['Date','PVV','PvdA-GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
-parties = ['PVV','PvdA-GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
+headers = ['Date','PVV','PvdA','GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
+parties = ['PVV','PvdA','GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
 data2.columns = headers
 
 data2['Date2'] = data2['Date'].str.split('–').str[1]
@@ -62,9 +34,37 @@ for z in parties:
   data2[z] = [x.replace('–',str(np.NaN)) for x in data2[z].astype(str)]
   data2[z] = [x.replace('–',str(np.NaN)) for x in data2[z].astype(str)]
 data2[parties] = data2[parties].astype(float)
+data2['PvdA']=np.where(data2['PvdA']==data2['GL'], 0, data2['PvdA'])
+Fusie=['PvdA','GL']
+data2[Fusie] = data2[Fusie].astype(float)
+data2['PvdA-GL'] = data2[Fusie].sum(axis=1)
+data2 = data2.drop(Fusie, axis=1)
 
 data2 = data2[['Date','PVV','PvdA-GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']]
 
+
+
+# '''CORRECTION'''
+# headers = ['Date','PVV','PvdA-GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
+# parties = ['PVV','PvdA-GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']
+# data2.columns = headers
+# 
+# data2['Date2'] = data2['Date'].str.split('–').str[1]
+# data2.Date2.fillna(data2['Date'].str.split('-').str[1], inplace=True)
+# data2.Date2.fillna(data2.Date, inplace=True)
+# data2.Date = data2.Date2
+# data2 = data2.drop(['Date2'],axis=1)
+# data2.Date = data2['Date'].astype(str)
+# data2.Date = data2.Date.apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
+# data2 = data2[data2['PVV'] != data2['SGP']]
+# for z in parties:
+#   data2[z] = [p.sub('', x) for x in data2[z].astype(str)]
+#   data2[z] = [x.replace('–',str(np.NaN)) for x in data2[z].astype(str)]
+#   data2[z] = [x.replace('–',str(np.NaN)) for x in data2[z].astype(str)]
+# data2[parties] = data2[parties].astype(float)
+# 
+# data2 = data2[['Date','PVV','PvdA-GL','VVD','NSC','D66','BBB','CDA','SP','DENK','PvdD','FvD','SGP','CU','Volt','JA21']]
+# 
 
 
 data2.to_csv('Dutch/poll_new.csv', index=False)
--- Dutch/poll_long.R
+++ Dutch/poll_long.R
@@ -37,7 +37,7 @@ plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
                                 "#45B6B1","#8C2591","#99C11A",
                                 "#FBFD00","#162141","#f0c400"))+
   geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.35,linewidth=0.75, data=d_old[d_old$Date!=election,])+
-  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.75,linewidth=0.75, data=d_new)+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=1,linewidth=0.75, data=d_new)+
   # geom_line(linewidth=0.75,data=d_new)+
   theme(axis.title=element_blank(),legend.title = element_blank(),
         legend.key.size = unit(2, 'lines'),
