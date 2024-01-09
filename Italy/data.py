ac40f866aeae94b1e1068e8572d0d1421298c40a
HarviLehal <sma18hsl@liverpool.ac.uk>
2024-01-09 23:42:27 +0000

    New Polls 09-I-2024

--- Italy/data.py
+++ Italy/data.py
@@ -15,13 +15,13 @@ df=pd.read_html(str(tables))
 headers = ['Date','FdI','PD','M5S','Lega','FI','A','IV','AVS','+E','Italexit']
 parties = ['FdI','PD','M5S','Lega','FI','A','IV','+E','Italexit']
 d = {}
-for i in range(1):
+for i in range(2):
   d[i]=pd.DataFrame(df[i])
   d[i]=d[i].drop(["Polling firm","Sample size","UP","DSP","NM","Others","Lead"], axis=1)
   d[i].columns = headers
   d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
   d[i].Date2.fillna(d[i].Date, inplace=True)
-  d[i]['Date2'] = [x+ str(2023-i) for x in d[i]['Date2'].astype(str)]
+  d[i]['Date2'] = [x+ str(2024-i) for x in d[i]['Date2'].astype(str)]
   d[i]['Date'] = d[i]['Date2']
   d[i] = d[i].drop(['Date2'], axis=1)
   d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
@@ -32,17 +32,18 @@ split_date = '15 Apr 2023'
 split_date=dateparser.parse(split_date)
 
 c={}
-c[0]=d[0][(pd.to_datetime(d[0]["Date"]) > split_date)]
-c[1]=d[0][(pd.to_datetime(d[0]["Date"]) < split_date)]
+c[0]=d[0]
+c[1]=d[1][(pd.to_datetime(d[1]["Date"]) > split_date)]
+c[2]=d[1][(pd.to_datetime(d[1]["Date"]) < split_date)]
 
-c[1]['A-IV']=np.where(c[1]['A']==c[1]['IV'],c[1]['A'],c[1]['A']+c[1]['IV'])
+c[2]['A-IV']=np.where(c[2]['A']==c[2]['IV'],c[2]['A'],c[2]['A']+c[2]['IV'])
 threeway = ['A','IV']
-c[1] = c[1].drop(threeway, axis=1)
+c[2] = c[2].drop(threeway, axis=1)
 
 headers = ['Date','FdI','PD','M5S','Lega','FI','A-IV','AVS','+E','Italexit']
 parties = ['FdI','PD','M5S','Lega','FI','A-IV','+E','Italexit']
 for j in range(1):
-  i = j+1
+  i = j+2
   c[i+2]=pd.DataFrame(df[i])
   c[i+2]=c[i+2].drop(["Polling firm","Sample size","UP","ISP","NM","Others","Lead"], axis=1)
   c[i+2].columns = headers
