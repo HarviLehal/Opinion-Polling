b7f1837b650984c20835e5a43a680da39a4f7eb7
HarviLehal <sma18hsl@liverpool.ac.uk>
2024-03-31 23:49:04 +0100

    Turkey Added

--- Turkish/poll.R
+++ Turkish/poll.R
@@ -7,38 +7,101 @@ library(Cairo)
 library(reshape2)
 library(readr)
 library(formattable)
+library(svglite)
+library(Rcpp)
+library(ggpubr)
+library(zoo)
+library(tidyverse)
+library(data.table)
+library(hrbrthemes)
+library(ggbreak)
 
-py_run_file("Turkish/data2.py")
-poll2 <- read_csv("Turkish/poll2.csv")
+py_run_file("Turkish/data.py")
+poll <- read_csv("Turkish/poll.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+d$value<-as.numeric(d$value)/100
+d$value<-formattable::percent(d$value)
 
-d2 <- reshape2::melt(poll2, id.vars="Date")
-d2$value<-as.numeric(d2$value)/100
-d2$value<-formattable::percent(d2$value)
-election<-as.Date("14 05 2023", "%d %m %Y")
-old<-min(d2$Date)
-d2$Date<-as.Date(d2$Date)
-h <- formattable::percent(0.5)
+election<-max(d$Date)+14
+old <-min(d$Date)
 
 # MAIN GRAPH
 
-plot2<-ggplot(data=d2,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(size=1, data=d2[d2$Date!=old,],alpha=0.75)+
-  scale_color_manual(values = c("#FFCC00","#3db5e6","#870000","#006aa7", "#2db34a" ,"#ff5f5f","#0d5ca6","#ed1c24","#951b88"))+
-  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.4,linewidth=0.75, data=d2[d2$Date!=old,])+
-  theme(axis.title=element_blank(),
-        legend.title = element_blank(),
+plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old|d$Date!=election,],alpha=0.25)+
+  scale_color_manual(values = c("#FFCC00","#ed1c24","#870000","#3db5e6","#951b88","#007d60","#404040","#b61f23"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.75,linewidth=0.75, data=d[d$Date!=old&d$Date!=election,])+
+  # bbplot::bbc_style()+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
         legend.key.size = unit(2, 'lines'),
-        legend.text = element_text(size=16))+
+        legend.position = "none")+
   scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
-  geom_hline(aes(yintercept=h))+
-  geom_text(aes((election),h,label = "50%",hjust=1 ,vjust = -1),colour="#56595c")+
-  geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
-  xlim(min(d2$Date), election)+
-  geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
-  geom_point(data=d2[d2$Date==old,],size=5, shape=18, alpha=0.5)+
-  geom_point(data=d2[d2$Date==old,],size=5.25, shape=5, alpha=0.5)
+  xlim(min(d$Date), election)+
+  geom_vline(xintercept=old,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=election,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)
+plot1
 
-ggsave(plot=plot2, file="Turkish/plot2.png",width = 15, height = 7.5, type = "cairo-png")
 
-# EXPERIMENTAL THINGS
+poll <- read_csv("Turkish/poll.csv")
+poll$Date <- as.Date(poll$Date, "%d %b %Y")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(sub("%","",as.character(x)))))
+d2 <- poll[poll$Date==min(poll$Date),]
+poll<-poll[poll$Date>(max(poll$Date)-14),]
+d1 <- colMeans(poll[-1],na.rm=TRUE)
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+d2 <- as.data.frame(d2)
 
+d1 <- reshape2::melt(d1, id.vars="Date")
+d1$value<-as.numeric(d1$value)/100
+d1$value<-formattable::percent(d1$value, digits = 1)
+
+d2 <- reshape2::melt(d2, id.vars="Date")
+d2$value<-as.numeric(d2$value)/100
+d2$value<-formattable::percent(d2$value, digits = 1)
+
+d3<-rbind(d2,d1)
+
+
+plot2<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+  geom_bar(stat="identity",width=0.9, position=position_dodge())+
+  scale_fill_manual(values = c("#ffe066","#FFCC00",
+                               "#f4777c","#ed1c24",
+                               "#b76666","#870000",
+                               "#8bd3f0","#3db5e6",
+                               "#bf76b8","#951b88",
+                               "#66b1a0","#007d60",
+                               "#8c8c8c","#404040",
+                               "#d3797b","#b61f23"))+
+  geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value, ""), digits = 1),
+                y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
+                y = 0),
+            hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+  ggtitle('14 day Average \n (2023 Election)')+
+  scale_x_discrete(limits = rev(levels(d3$variable)),labels = label_wrap(8))+
+  coord_flip()
+plot2
+
+
+
+plot<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.6))
+plot
+
+
+ggsave(plot=plot, file="Turkish/plot.png",width = 15, height = 7.5, type = "cairo-png")
