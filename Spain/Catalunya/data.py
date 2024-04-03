5f9d068f1570e21a956300baf7a7f8eeef598052
HarviLehal <sma18hsl@liverpool.ac.uk>
2024-04-03 22:53:36 +0100

    New Polls 03-IV-2024

--- Spain/Catalunya/data.py
+++ Spain/Catalunya/data.py
@@ -51,6 +51,14 @@ D.drop(D.index[[-1,-3]],inplace=True)
 
 D.to_csv('Spain/Catalunya/poll.csv', index=False)
 
+Ind = ['ERC','Junts','CUP','AC']
+Fed = ['PSC','VOX','ECP','Cs','PP']
+D['Pro-Independence (ERC + Junts + CUP + AC)'] = D[Ind].sum(axis=1)
+D['Rest (PSC + VOX + ECP + Cs + PP)'] = D[Fed].sum(axis=1)
+D = D.drop(Ind + Fed, axis=1)
+
+D.to_csv('Spain/Catalunya/poll2.csv', index=False)
+
 
 
 
@@ -90,3 +98,206 @@ D['Rest (PSC + VOX + ECP + Cs + PP)'] = D[Fed].sum(axis=1)
 D = D.drop(Ind + Fed, axis=1)
 
 D.to_csv('Spain/Catalunya/seats2.csv', index=False)
+
+
+# best case for Independence
+D = pd.concat(d.values(), ignore_index=True)
+D = D.replace(r'^\s*$', np.NaN, regex=True)
+D[parties] = D[parties].astype(float)
+D.drop(D.index[[-1,-3]],inplace=True)
+
+Ind = ['ERC','Junts','CUP','AC']
+Fed = ['PSC','VOX','ECP','Cs','PP']
+D['Pro-Independence (ERC + Junts + CUP + AC)'] = D[Ind].sum(axis=1)
+D['Rest (PSC + VOX + ECP + Cs + PP)'] = 134 - D['Pro-Independence (ERC + Junts + CUP + AC)']
+D = D.drop(Ind + Fed, axis=1)
+
+D.to_csv('Spain/Catalunya/seats_ind_best.csv', index=False)
+
+
+D = pd.concat(d.values(), ignore_index=True)
+D = D.replace(r'^\s*$', np.NaN, regex=True)
+D[parties] = D[parties].astype(float)
+D.drop(D.index[[-1,-3]],inplace=True)
+
+Ind = ['ERC','Junts','CUP','AC']
+Fed = ['PSC','VOX','ECP','Cs','PP']
+D['Rest (PSC + VOX + ECP + Cs + PP)'] = D[Fed].sum(axis=1)
+D['Pro-Independence (ERC + Junts + CUP + AC)'] = 134 - D['Rest (PSC + VOX + ECP + Cs + PP)']
+D = D.drop(Ind + Fed, axis=1)
+
+D.to_csv('Spain/Catalunya/seats_rest_best.csv', index=False)
+
+# worst case for Independence
+
+
+
+d = {}
+for i in range(1):
+  d[i]=pd.DataFrame(df[3])
+  d[i].columns = headers
+  d[i]=d[i].drop(drops, axis=1)
+  d[i] = d[i][d[i]['Date'] != '23 Jul 2023']
+  d[i] = d[i][d[i]['Date'] != '28 May 2023']
+  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
+  d[i].Date2.fillna(d[i].Date, inplace=True)
+  d[i]['Date2'] = [x for x in d[i]['Date2'].astype(str)]
+  d[i]['Date'] = d[i]['Date2']
+  d[i] = d[i].drop(['Date2'], axis=1)
+  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
+  d[i] = d[i][d[i]['PSC'] != d[i]['Cs']]
+  for z in parties:
+    d[i][z] = [p.sub('', x) for x in d[i][z].astype(str)]
+    d[i][z] = d[i][z].str.split(' ').str[1]
+    d[i][z] = [x.split('/')[0] if len(x.split('/')) > 1 else x for x in d[i][z].astype(str)]
+    d[i][z] = [x.replace('–',str()) for x in d[i][z].astype(str)]
+    d[i][z] = [x.replace('?',str(np.NaN)) for x in d[i][z].astype(str)]
+
+D = pd.concat(d.values(), ignore_index=True)
+D = D.replace(r'^\s*$', np.NaN, regex=True)
+D[parties] = D[parties].astype(float)
+D.drop(D.index[[-1,-3]],inplace=True)
+
+Ind = ['ERC','Junts','CUP','AC']
+Fed = ['PSC','VOX','ECP','Cs','PP']
+D['Pro-Independence (ERC + Junts + CUP + AC)'] = D[Ind].sum(axis=1)
+D['Rest (PSC + VOX + ECP + Cs + PP)'] = 134 - D['Pro-Independence (ERC + Junts + CUP + AC)']
+D = D.drop(Ind + Fed, axis=1)
+
+D.to_csv('Spain/Catalunya/seats_ind_worst.csv', index=False)
+
+
+
+
+D = pd.concat(d.values(), ignore_index=True)
+D = D.replace(r'^\s*$', np.NaN, regex=True)
+D[parties] = D[parties].astype(float)
+D.drop(D.index[[-1,-3]],inplace=True)
+
+Ind = ['ERC','Junts','CUP','AC']
+Fed = ['PSC','VOX','ECP','Cs','PP']
+D['Rest (PSC + VOX + ECP + Cs + PP)'] = D[Fed].sum(axis=1)
+D['Pro-Independence (ERC + Junts + CUP + AC)'] = 134 - D['Rest (PSC + VOX + ECP + Cs + PP)']
+D = D.drop(Ind + Fed, axis=1)
+
+D.to_csv('Spain/Catalunya/seats_rest_worst.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d = {}
+for i in range(1):
+  d[i]=pd.DataFrame(df[3])
+  d[i].columns = headers
+  d[i]=d[i].drop(drops, axis=1)
+  d[i] = d[i][d[i]['Date'] != '23 Jul 2023']
+  d[i] = d[i][d[i]['Date'] != '28 May 2023']
+  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
+  d[i].Date2.fillna(d[i].Date, inplace=True)
+  d[i]['Date2'] = [x for x in d[i]['Date2'].astype(str)]
+  d[i]['Date'] = d[i]['Date2']
+  d[i] = d[i].drop(['Date2'], axis=1)
+  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
+  d[i] = d[i][d[i]['PSC'] != d[i]['Cs']]
+  for z in parties:
+    d[i][z] = [p.sub('', x) for x in d[i][z].astype(str)]
+    d[i][z] = d[i][z].str.split(' ').str[1]
+    d[i][z] = [x.split('/')[0] if len(x.split('/')) > 1 else x for x in d[i][z].astype(str)]
+    d[i][z] = [x.replace('–',str()) for x in d[i][z].astype(str)]
+    d[i][z] = [x.replace('?',str(np.NaN)) for x in d[i][z].astype(str)]
+
+D = pd.concat(d.values(), ignore_index=True)
+D = D.replace(r'^\s*$', np.NaN, regex=True)
+D[parties] = D[parties].astype(float)
+D.drop(D.index[[-1,-3]],inplace=True)
+
+Ind = ['ERC','Junts','CUP','AC']
+Fed = ['PSC','VOX','ECP','Cs','PP']
+D['Pro-Independence (ERC + Junts + CUP + AC)'] = D[Ind].sum(axis=1)
+D['Rest (PSC + VOX + ECP + Cs + PP)'] = D[Fed].sum(axis=1)
+D = D.drop(Ind + Fed, axis=1)
+
+
+d = {}
+for i in range(1):
+  d[i]=pd.DataFrame(df[3])
+  d[i].columns = headers
+  d[i]=d[i].drop(drops, axis=1)
+  d[i] = d[i][d[i]['Date'] != '23 Jul 2023']
+  d[i] = d[i][d[i]['Date'] != '28 May 2023']
+  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
+  d[i].Date2.fillna(d[i].Date, inplace=True)
+  d[i]['Date2'] = [x for x in d[i]['Date2'].astype(str)]
+  d[i]['Date'] = d[i]['Date2']
+  d[i] = d[i].drop(['Date2'], axis=1)
+  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
+  d[i] = d[i][d[i]['PSC'] != d[i]['Cs']]
+  for z in parties:
+    d[i][z] = [p.sub('', x) for x in d[i][z].astype(str)]
+    d[i][z] = d[i][z].str.split(' ').str[1]
+    d[i][z] = [x.split('/')[1] if len(x.split('/')) > 1 else x for x in d[i][z].astype(str)]
+    d[i][z] = [x.replace('–',str()) for x in d[i][z].astype(str)]
+    d[i][z] = [x.replace('?',str(np.NaN)) for x in d[i][z].astype(str)]
+
+E = pd.concat(d.values(), ignore_index=True)
+E = E.replace(r'^\s*$', np.NaN, regex=True)
+E[parties] = E[parties].astype(float)
+E.drop(E.index[[-1,-3]],inplace=True)
+
+Ind = ['ERC','Junts','CUP','AC']
+Fed = ['PSC','VOX','ECP','Cs','PP']
+E['Pro-Independence (ERC + Junts + CUP + AC)'] = E[Ind].sum(axis=1)
+E['Rest (PSC + VOX + ECP + Cs + PP)'] = E[Fed].sum(axis=1)
+E = E.drop(Ind + Fed, axis=1)
+
+
+
+
+# D is lower, E is upper so we create F as the average between D and E of each column but the date stays the same
+
+parties = ['Pro-Independence (ERC + Junts + CUP + AC)','Rest (PSC + VOX + ECP + Cs + PP)']
+
+F = D.copy()
+F[parties] = (D[parties] + E[parties]) / 2
+
+# check if the sum of the two columns is 135 for each row
+print(F[parties].sum(axis=1))
+# convert to percentages and then back to seats
+totals = F[parties].sum(axis=1)
+# convert to percentages and then back to seats
+for party in parties:
+  F[party] = F[party] / totals * 135
+# check if the sum of the two columns is 135 for each row
+print(F[parties].sum(axis=1))
+
+
+F.to_csv('Spain/Catalunya/seats_avg.csv', index=False)
+
+
+
+
+
+
+
+
--- Spain/Catalunya/poll3.R
+++ Spain/Catalunya/poll3.R
@@ -14,41 +14,41 @@ library(zoo)
 library(tidyverse)
 library(data.table)
 library(hrbrthemes)
-poll <- read_csv("Spain/Catalunya/seats2.csv")
-d <- reshape2::melt(poll, id.vars="Date")
 
+
+poll <- read_csv("Spain/Catalunya/seats2.csv")
+d2 <- poll[poll$Date==min(poll$Date),]
+d2 <- as.data.frame(d2)
+d2 <- reshape2::melt(d2, id.vars="Date")
+
+# BEST IND POLLS
+
+poll <- read_csv("Spain/Catalunya/seats_ind_best.csv")
+d <- reshape2::melt(poll, id.vars="Date")
 election<-as.Date("18 05 2024", "%d %m %Y")
 old <-min(d$Date)
-# start<-as.Date("01 01 2024", "%d %m %Y")
-# d<-d[d$Date>start,]
-
-# LOESS GRAPH
 
 plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_point(size=1, data=d[d$Date!=old&d$Date!=election,],alpha=0.25)+
   scale_color_manual(values = c("#ffb232","#ef1c27"))+
   geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.5,linewidth=0.75, data=d[d$Date!=old&d$Date!=election,])+
-  # geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.5,linewidth=0.75, data=new[new$Date!=old,])+
-  # geom_smooth(method = "lm",formula=y ~ x + I(x^2),fullrange=FALSE,se=FALSE, linewidth=0.75, data=new2[new2$Date!=old,])+
-  # bbplot::bbc_style()+
   theme(axis.title=element_blank(),legend.title = element_blank(),
         legend.key.size = unit(2, 'lines'),
         legend.position = "none")+
-  # scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
   xlim(min(d$Date)-0.725, election)+
   geom_vline(xintercept=old,
              linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   geom_vline(xintercept=election,
              linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
-  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)+
+  ggtitle('Independence Upper bound polls')
 plot1
 
-poll <- read_csv("Spain/Catalunya/seats2.csv")
+poll <- read_csv("Spain/Catalunya/seats_ind_best.csv")
 Date <- c(max(poll$Date))
 poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
   as.numeric(x)))
-d2 <- poll[poll$Date==min(poll$Date),]
 poll<-poll[poll$Date>(max(poll$Date)-14),]
 d1 <- round(colMeans(poll[-1],na.rm=TRUE), digits=0)
 d1 <- as.data.frame(d1)
@@ -56,12 +56,8 @@ d1 <- t(d1)
 d1 <- cbind(Date, d1)
 d1 <- as.data.frame(d1)
 d1$Date <- as.Date(d1$Date)
-d2 <- as.data.frame(d2)
 
 d1 <- reshape2::melt(d1, id.vars="Date")
-
-d2 <- reshape2::melt(d2, id.vars="Date")
-
 d3<-rbind(d2,d1)
 
 
@@ -84,7 +80,353 @@ plot2<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), 
 plot2
 
 
+# WORST IND POLLS
+
+poll <- read_csv("Spain/Catalunya/seats_ind_worst.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+election<-as.Date("18 05 2024", "%d %m %Y")
+old <-min(d$Date)
+
+plot3<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old&d$Date!=election,],alpha=0.25)+
+  scale_color_manual(values = c("#ffb232","#ef1c27"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.5,linewidth=0.75, data=d[d$Date!=old&d$Date!=election,])+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")+
+  xlim(min(d$Date)-0.725, election)+
+  geom_vline(xintercept=old,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=election,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)+
+  ggtitle('Independence Lower bound polls')
+plot3
+
+poll <- read_csv("Spain/Catalunya/seats_ind_worst.csv")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(x)))
+poll<-poll[poll$Date>(max(poll$Date)-14),]
+d1 <- round(colMeans(poll[-1],na.rm=TRUE), digits=0)
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+
+d1 <- reshape2::melt(d1, id.vars="Date")
+d3<-rbind(d2,d1)
+
+
+plot4<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+  geom_bar(stat="identity",width=0.9, position=position_dodge())+
+  scale_fill_manual(values = c("#ffc970","#ffb232",
+                               "#f46068","#ef1c27"))+
+  geom_text(aes(label = ifelse(d3$Date != min(d3$Date), d3$value, ""),y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
+                y = 0),hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+  ggtitle('14 day Average \n (2021 Election)')+
+  scale_x_discrete(limits = rev(levels(d3$variable)),labels = label_wrap(8))+
+  coord_flip()
+plot4
+
+
+# BEST REST POLLS
+
+poll <- read_csv("Spain/Catalunya/seats_rest_best.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+election<-as.Date("18 05 2024", "%d %m %Y")
+old <-min(d$Date)
+
+plot5<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old&d$Date!=election,],alpha=0.25)+
+  scale_color_manual(values = c("#ef1c27","#ffb232"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.5,linewidth=0.75, data=d[d$Date!=old&d$Date!=election,])+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")+
+  xlim(min(d$Date)-0.725, election)+
+  geom_vline(xintercept=old,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=election,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)+
+  ggtitle('Rest Upper bound polls')
+plot5
+
+poll <- read_csv("Spain/Catalunya/seats_rest_best.csv")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(x)))
+poll<-poll[poll$Date>(max(poll$Date)-14),]
+d1 <- round(colMeans(poll[-1],na.rm=TRUE), digits=0)
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+
+d1 <- reshape2::melt(d1, id.vars="Date")
+d3<-rbind(d2,d1)
+
+
+plot6<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+  geom_bar(stat="identity",width=0.9, position=position_dodge())+
+  scale_fill_manual(values = c("#ffc970","#ffb232",
+                               "#f46068","#ef1c27"))+
+  geom_text(aes(label = ifelse(d3$Date != min(d3$Date), d3$value, ""),y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
+                y = 0),hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+  ggtitle('14 day Average \n (2021 Election)')+
+  scale_x_discrete(limits = rev(levels(d3$variable)),labels = label_wrap(8))+
+  coord_flip()
+plot6
+
+
+# WORST REST POLLS
+
+poll <- read_csv("Spain/Catalunya/seats_rest_worst.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+election<-as.Date("18 05 2024", "%d %m %Y")
+old <-min(d$Date)
+
+plot7<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old&d$Date!=election,],alpha=0.25)+
+  scale_color_manual(values = c("#ef1c27","#ffb232"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.5,linewidth=0.75, data=d[d$Date!=old&d$Date!=election,])+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")+
+  xlim(min(d$Date)-0.725, election)+
+  geom_vline(xintercept=old,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=election,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)+
+  ggtitle('Rest Lower bound polls')
+plot7
+
+poll <- read_csv("Spain/Catalunya/seats_rest_worst.csv")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(x)))
+poll<-poll[poll$Date>(max(poll$Date)-14),]
+d1 <- round(colMeans(poll[-1],na.rm=TRUE), digits=0)
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+
+d1 <- reshape2::melt(d1, id.vars="Date")
+d3<-rbind(d2,d1)
+
+
+plot8<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+  geom_bar(stat="identity",width=0.9, position=position_dodge())+
+  scale_fill_manual(values = c("#ffc970","#ffb232",
+                               "#f46068","#ef1c27"))+
+  geom_text(aes(label = ifelse(d3$Date != min(d3$Date), d3$value, ""),y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
+                y = 0),hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+  ggtitle('14 day Average \n (2021 Election)')+
+  scale_x_discrete(limits = rev(levels(d3$variable)),labels = label_wrap(8))+
+  coord_flip()
+plot8
+
+
+
+
+
+
+
+
+
+
+
+
+
+plot<-ggarrange(plot1, plot2, plot3, plot4, plot5, plot6, plot7, plot8 ,ncol = 4, nrow = 2,widths=c(2,0.6,2,0.6))
+plot
+
+ggsave(plot=plot, file="Spain/Catalunya/seat_projections.png",width = 30, height = 15, type="cairo-png")
+
+
+
+
+
+
+
+
+
+
+
+poll <- read_csv("Spain/Catalunya/seats2.csv")
+d2 <- poll[poll$Date==min(poll$Date),]
+d2 <- as.data.frame(d2)
+d2 <- reshape2::melt(d2, id.vars="Date")
+
+# OG POLLS
+
+poll <- read_csv("Spain/Catalunya/seats2.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+election<-as.Date("18 05 2024", "%d %m %Y")
+old <-min(d$Date)
+
+plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old&d$Date!=election,],alpha=0.25)+
+  scale_color_manual(values = c("#ffb232","#ef1c27"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.5,linewidth=0.75, data=d[d$Date!=old&d$Date!=election,])+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")+
+  xlim(min(d$Date)-0.725, election)+
+  geom_vline(xintercept=old,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=election,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)
+plot1
+
+poll <- read_csv("Spain/Catalunya/seats2.csv")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(x)))
+poll<-poll[poll$Date>(max(poll$Date)-14),]
+d1 <- round(colMeans(poll[-1],na.rm=TRUE), digits=0)
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+
+d1 <- reshape2::melt(d1, id.vars="Date")
+d3<-rbind(d2,d1)
+
+
+plot2<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+  geom_bar(stat="identity",width=0.9, position=position_dodge())+
+  scale_fill_manual(values = c("#ffc970","#ffb232",
+                               "#f46068","#ef1c27"))+
+  geom_text(aes(label = ifelse(d3$Date != min(d3$Date), d3$value, ""),y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
+                y = 0),hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+  ggtitle('14 day Average \n (2021 Election)')+
+  scale_x_discrete(limits = rev(levels(d3$variable)),labels = label_wrap(8))+
+  coord_flip()
+plot2
+
 plot<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.6))
 plot
 
 ggsave(plot=plot, file="Spain/Catalunya/seats2.png",width = 15, height = 7.5, type="cairo-png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+poll <- read_csv("Spain/Catalunya/seats2.csv")
+d2 <- poll[poll$Date==min(poll$Date),]
+d2 <- as.data.frame(d2)
+d2 <- reshape2::melt(d2, id.vars="Date")
+
+# OG POLLS
+
+poll <- read_csv("Spain/Catalunya/seats_avg.csv")
+d <- reshape2::melt(poll, id.vars="Date")
+election<-as.Date("18 05 2024", "%d %m %Y")
+old <-min(d$Date)
+
+plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
+  geom_point(size=1, data=d[d$Date!=old&d$Date!=election,],alpha=0.25)+
+  scale_color_manual(values = c("#ffb232","#ef1c27"))+
+  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.5,linewidth=0.75, data=d[d$Date!=old&d$Date!=election,])+
+  theme(axis.title=element_blank(),legend.title = element_blank(),
+        legend.key.size = unit(2, 'lines'),
+        legend.position = "none")+
+  xlim(min(d$Date)-0.725, election)+
+  geom_vline(xintercept=old,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_vline(xintercept=election,
+             linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5, shape=18, alpha=0.5)+
+  geom_point(data=d[d$Date==old|d$Date==election,],size=5.25, shape=5, alpha=0.5)
+plot1
+
+poll <- read_csv("Spain/Catalunya/seats_avg.csv")
+Date <- c(max(poll$Date))
+poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
+  as.numeric(x)))
+poll<-poll[poll$Date>(max(poll$Date)-14),]
+d1 <- round(colMeans(poll[-1],na.rm=TRUE), digits=0)
+d1 <- as.data.frame(d1)
+d1 <- t(d1)
+d1 <- cbind(Date, d1)
+d1 <- as.data.frame(d1)
+d1$Date <- as.Date(d1$Date)
+
+d1 <- reshape2::melt(d1, id.vars="Date")
+d3<-rbind(d2,d1)
+
+
+plot2<-ggplot(data=d3, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
+  geom_bar(stat="identity",width=0.9, position=position_dodge())+
+  scale_fill_manual(values = c("#ffc970","#ffb232",
+                               "#f46068","#ef1c27"))+
+  geom_text(aes(label = ifelse(d3$Date != min(d3$Date), d3$value, ""),y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
+                y = 0),hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+  ggtitle('14 day Average \n (2021 Election)')+
+  scale_x_discrete(limits = rev(levels(d3$variable)),labels = label_wrap(8))+
+  coord_flip()
+plot2
+
+plot<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.6))
+plot
+
+ggsave(plot=plot, file="Spain/Catalunya/seats_average.png",width = 15, height = 7.5, type="cairo-png")
