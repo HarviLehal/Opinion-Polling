0ad22f6375e55cfd174bd35f4a31ebf38a7029d6
HarviLehal <sma18hsl@liverpool.ac.uk>
2024-06-11 00:06:56 +0100

    Fixed Norway

--- Norwegian/poll.R
+++ Norwegian/poll.R
@@ -30,49 +30,35 @@ plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_point(size=1, data=d[d$Date!=old,],alpha=0.5)+
   scale_color_manual(values = c("#D82C3C","#AC347D","#6F9323","#CB182D","#2D843B",
                                 "#236666","#F9DA5A","#3064F1","#2760A7","#08b4cc"))+
-  geom_smooth(method="loess",fullrange=TRUE,se=FALSE,span=0.25,linewidth=0.75, data=d[d$Date!=old,])+
-  # bbplot::bbc_style()+
+  geom_smooth(method="loess",fullrange=FALSE,se=FALSE,span=0.25,linewidth=0.75, data=d[d$Date!=old,])+
+  theme_minimal()+
   theme(axis.title=element_blank(),legend.title = element_blank(),
         legend.key.size = unit(2, 'lines'),
-        legend.position = "none")+
+        legend.position = "none",
+        axis.text.x = element_text(face="bold"),
+        axis.text.y = element_text(face="bold"),
+        plot.title = element_text(face="bold"),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
   scale_y_continuous(name="Vote",labels = scales::percent_format(accuracy = 5L),breaks=seq(0,0.6,0.05))+
   geom_vline(xintercept=election, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   xlim(min(d$Date), election)+
   geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
-  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)
+  geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)+
+  scale_x_date(date_breaks = "2 month", date_labels =  "%b %Y",limits = c(old,election),guide = guide_axis(angle = -90))+
+  ggtitle('Opinion Polling for the 2025 Norwegian Parliamentary Election')
 
-# poll2 <- setDT(poll)
-# cols <- names(poll2)[-1]
-# poll2[, paste0(cols, "_30_avg") := 
-#      lapply(.SD, function(x) fcoalesce(frollmean(x, n=30:1, na.rm=TRUE))), 
-#    .SDcols = cols]
-# poll2 <- poll2[,-2:-10]
-# d2=poll2[poll2$Date==max(poll2$Date),]
-# colnames(d2)[2] = "R"
-# colnames(d2)[3] = "SV"
-# colnames(d2)[4] = "MDG"
-# colnames(d2)[5] = "Ap"
-# colnames(d2)[6] = "Sp"
-# colnames(d2)[7] = "V"
-# colnames(d2)[8] = "KrF"
-# colnames(d2)[9] = "H"
-# colnames(d2)[10] = "FrP"
-# 
-# 
-# d2 <- reshape2::melt(d2, id.vars="Date")
-# d2$value<-as.numeric(d2$value)/100
-# d2$value<-formattable::percent(d2$value)
-# d2<-rbind(d2,d[d$Date==old,])
+
 
 poll <- read_csv("Norwegian/poll.csv")
-# poll$Date <- as.Date(poll$Date, "%d %b %Y")
+poll$Date <- as.Date(poll$Date, "%d %b %Y")
 Date <- c(max(poll$Date))
 poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
   as.numeric(sub("%","",as.character(x)))))
 d2 <- poll[poll$Date==min(poll$Date),]
-poll<-poll[poll$Date>(max(poll$Date)-30),]
-d1 <- colMeans(poll[-1])
+poll<-poll[poll$Date>(max(poll$Date)-14),]
+d1 <- colMeans(poll[-1],na.rm = TRUE)
 d1 <- as.data.frame(d1)
 d1 <- t(d1)
 d1 <- cbind(Date, d1)
@@ -99,20 +85,22 @@ scale_fill_manual(values = c("#ed8e98","#D82C3C","#d68bba","#AC347D",
                              "#b1c97d","#6F9323","#e6818c","#CB182D",
                              "#82c28d","#2D843B","#78b3b3","#236666",
                              "#fcedac","#F9DA5A","#94b0f7","#3064F1",
-                             "#81a6d4","#2760A7","#6bd2e0","#08b4cc"))+
-geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value, ""), digits = 1),
-              y = 0),
-          hjust=0, color="#000000",position = position_dodge(1), size=3.5)+
-geom_text(aes(label = ifelse(d3$Date == min(d3$Date),paste("(",d2$value,")"),""),
-              y = 0),
-          hjust=0, color="#404040", position = position_dodge(1), size=3.5)+
-theme_minimal()+
-theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
-      panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-      panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
-      plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
-ggtitle('30 day average \n (2021 Result)')+
-coord_flip()
+                             "#81a6d3","#2760A7","#6bd2e0","#08b4cc"))+
+  geom_text(aes(label = formattable::percent(ifelse(d3$Date != min(d3$Date), d3$value, ""), digits = 1),y = 0),
+            hjust=0, color="#000000",position = position_dodge(1), size=3.5, fontface="bold")+
+  geom_text(aes(label = ifelse(d3$Date == min(d3$Date),ifelse(is.na(d3$value)==TRUE,paste("(New)"),(paste("(",formattable::percent(d3$value,digits=1),")"))),""),y = 0),
+            hjust=0, color="#404040", position = position_dodge(1), size=3.5, fontface="bold")+
+  theme_minimal()+
+  theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
+        axis.text.y = element_text(face="bold"),
+        plot.title = element_text(face="bold"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
+        plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
+  ggtitle('14 day average \n (2021 Result)')+
+  scale_x_discrete(limits = (levels(d3$variable)))+
+  coord_flip()
+
 
 
 plot<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.5))
