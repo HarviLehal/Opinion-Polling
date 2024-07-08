f20cb5f622695747e9c7eae11bf0ab7a9c43a48d
HarviLehal <sma18hsl@liverpool.ac.uk>
2024-07-08 21:08:26 +0100

    New Polls 08-VII-2024

--- Dutch/poll.R
+++ Dutch/poll.R
@@ -46,7 +46,7 @@ plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
   geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)+
-  scale_x_date(date_breaks = "1 month", date_labels =  "%m %Y",limits = c(old,max),guide = guide_axis(angle = -90))+
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y",limits = c(old,max),guide = guide_axis(angle = -90))+
   ggtitle('Government-Opposition Seat Projections for the Next Dutch General Election')
 
 plot1
@@ -72,7 +72,7 @@ plot2<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_vline(xintercept=old, linetype="solid", color = "#56595c", alpha=0.5, size=0.75)+
   geom_point(data=d[d$Date==old,],size=5, shape=18, alpha=0.5)+
   geom_point(data=d[d$Date==old,],size=5.25, shape=5, alpha=0.5)+
-  scale_x_date(date_breaks = "1 month", date_labels =  "%m %Y",limits = c(old,max),guide = guide_axis(angle = -90))+
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y",limits = c(old,max),guide = guide_axis(angle = -90))+
   ggtitle('Government-Opposition Seat Projections for the Next Dutch General Election')
 
 plot2
