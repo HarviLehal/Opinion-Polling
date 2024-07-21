2f1857929cdbbf07304445d41777dc6789810989
HarviLehal <sma18hsl@liverpool.ac.uk>
2024-07-21 02:01:51 +0100

    Korea Added

--- Korea/data.py
+++ Korea/data.py
@@ -5,7 +5,7 @@ import numpy as np
 import dateparser
 import re
 
-wikiurl="https://en.wikipedia.org/wiki/Opinion_polling_for_the_2024_South_Korean_legislative_election"
+wikiurl="https://en.wikipedia.org/wiki/2028_South_Korean_legislative_election"
 table_class="wikitable sortable jquery-tablesorter"
 response=requests.get(wikiurl)
 print(response.status_code)
@@ -14,51 +14,13 @@ tables = soup.find_all('table',class_="wikitable")
 p = re.compile(r'\[[a-z]+\]')
 df=pd.read_html(str(tables))
 
-headers = ['Date','DPK','PPP','GJP','NRP','NFP','Other']
-parties = ['DPK','PPP','GJP','NRP','NFP','Other']
+headers = ['Date','DPK','PPP','RKP','NRP','PP','NFP','BIP','SDP','Other']
+parties = ['DPK','PPP','RKP','NRP','PP','NFP','BIP','SDP','Other']
 d = {}
 
-for i in range(2):
-  d[i]=pd.DataFrame(df[i])
-  d[i]=d[i].drop(['Polling firm','Sample size','Margin of error','Ind.','Und./ no ans.','Lead','RKP'], axis=1)
-  d[i].columns = headers
-  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
-  d[i].Date2.fillna(d[i]['Date'].str.split('-').str[1], inplace=True)
-  d[i].Date2.fillna(d[i].Date, inplace=True)
-  d[i]['Date2'] = [x for x in d[i]['Date2'].astype(str)]
-  d[i]['Date'] = d[i]['Date2']
-  d[i] = d[i].drop(['Date2'], axis=1)
-  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
-  d[i] = d[i][d[i]['DPK'] != d[i]['NFP']]
-  for z in parties:
-    d[i][z] = [p.sub('', x) for x in d[i][z].astype(str)]
-    d[i][z] = [x.replace('-',str(np.NaN)) for x in d[i][z]]
-    d[i][z] = [x.replace('—',str(np.NaN)) for x in d[i][z]]
-    d[i][z] = [x.replace('–',str(np.NaN)) for x in d[i][z]]
-    d[i][z] = d[i][z].astype('float').astype(str)
-
-D = pd.concat(d.values(), ignore_index=True)
-# new_row = pd.DataFrame({'Date': '15 March 2020','DPK':49.91,'PPP':41.46,'GJP':1.71,'NRP':np.nan,'NFP':np.nan,'Other':6.92}, index=[0])
-# D = pd.concat([new_row,D]).reset_index(drop=True)
-# D.Date=D.Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
-D.to_csv('Korea/poll.csv', index=False)
-
-
-
-# headers = ['Date','DAK','PFP','GJP','NRP','NFP','RKP','Other']
-# parties = ['DAK','PFP','GJP','NRP','NFP','RKP','Other']
-d = {}
-
-for i in range(2):
+for i in range(1):
   d[i]=pd.DataFrame(df[i+2])
-  if i ==0:
-    headers = ['Date','DAK','PFP','GJP','NRP','NFP','RKP','Other']
-    parties = ['DAK','PFP','GJP','NRP','NFP','RKP','Other']
-    d[i]=d[i].drop(['Polling firm','Sample size','Margin of error','LUP','Und./ no ans.','Lead'], axis=1)
-  else:
-    headers = ['Date','DAK','PFP','GJP','Other']
-    parties = ['DAK','PFP','GJP','Other']
-    d[i]=d[i].drop(['Polling firm','Sample size','Margin of error','LUP','Und./ no ans.','Lead'], axis=1)
+  d[i]=d[i].drop(['Polling firm','Sample size','Margin of error','Und./ no ans.','Lead'], axis=1)
   d[i].columns = headers
   d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
   d[i].Date2.fillna(d[i]['Date'].str.split('-').str[1], inplace=True)
@@ -67,7 +29,7 @@ for i in range(2):
   d[i]['Date'] = d[i]['Date2']
   d[i] = d[i].drop(['Date2'], axis=1)
   d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
-  d[i] = d[i][d[i]['DAK'] != d[i]['GJP']]
+  d[i] = d[i][d[i]['DPK'] != d[i]['PP']]
   for z in parties:
     d[i][z] = [p.sub('', x) for x in d[i][z].astype(str)]
     d[i][z] = [x.replace('-',str(np.NaN)) for x in d[i][z]]
@@ -76,7 +38,5 @@ for i in range(2):
     d[i][z] = d[i][z].astype('float').astype(str)
 
 D = pd.concat(d.values(), ignore_index=True)
-# new_row = pd.DataFrame({'Date': '15 March 2020','DAK':33.36,'PFP':33.84,'GJP':9.67,'NRP':np.nan,'NFP':np.nan,'RKP':np.nan,'Other':23.13}, index=[0])
-# D = pd.concat([new_row,D]).reset_index(drop=True)
-# D.Date=D.Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
-D.to_csv('Korea/poll2.csv', index=False)
+
+D.to_csv('Korea/poll.csv', index=False)
