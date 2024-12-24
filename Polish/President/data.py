ea325a1561d3c69f9c31c6e07625950ddaf010a4
HarviLehal <sma18hsl@liverpool.ac.uk>
2024-12-24 23:05:09 +0000

    New Polls 24-XII-2024

--- Polish/President/data.py
+++ Polish/President/data.py
@@ -14,13 +14,49 @@ tables = soup.find_all('table',class_="wikitable")
 df=pd.read_html(str(tables))
 p = re.compile(r'\[[a-z]+\]')
 
+headers = ['1','Date','2','Nawrocki','Trzaskowski','Hołownia','Biejat','Mentzen','Jakubiak','3','4','5']
+parties = ['Nawrocki','Trzaskowski','Hołownia','Biejat','Mentzen','Jakubiak']
+drops = ['1','2','3','4','5']
+d = {}
+for i in range(1):
+  # i=j+1
+  d[i]=pd.DataFrame(df[1])
+  d[i].columns = headers
+  d[i]=d[i].drop(drops, axis=1)
+  for z in parties:
+    d[i][z] = [p.sub('', x) for x in d[i][z].astype(str)]
+    d[i][z] = [x.replace('-',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('—',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('–',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('TBC',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('TBA',str(np.NaN)) for x in d[i][z]]
+    d[i][z] = [x.replace('?',str(np.NaN)) for x in d[i][z]]
+  d[i]['Date2'] = d[i]['Date'].str.split('–').str[1]
+  d[i].Date2.fillna(d[i]['Date'].str.split('-').str[1], inplace=True)
+  d[i].Date2.fillna(d[i].Date, inplace=True)
+  d[i]['Date'] = d[i]['Date2']
+  d[i] = d[i].drop(['Date2'], axis=1)
+  d[i].Date=d[i].Date.astype(str).apply(lambda x: dateparser.parse(x, settings={'PREFER_DAY_OF_MONTH': 'first'}))
+
+
+D = pd.concat(d.values(), ignore_index=True)
+for z in parties:
+  D[z] = D[z].astype(str)
+  D[z] = pd.to_numeric(D[z], errors='coerce')
+
+D=D.dropna(subset=['Date'])
+D=D.dropna(subset=['Trzaskowski'])
+
+D.to_csv('Polish/President/poll_new.csv', index=False)
+
+
 headers = ['1','Date','2','3','4','5','6','Morawiecki','Nawrocki','7','8','Trzaskowski','9','Hołownia','10','11','Dziemianowicz-Bąk','12','Biejat','13','Bosak','Mentzen','14','15','16']
 parties = ['Morawiecki','Nawrocki','Trzaskowski','Hołownia','Dziemianowicz-Bąk','Bosak','Mentzen']
 drops = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
 d = {}
 for i in range(1):
   # i=j+1
-  d[i]=pd.DataFrame(df[1])
+  d[i]=pd.DataFrame(df[2])
   d[i].columns = headers
   d[i]=d[i].drop(drops, axis=1)
   for z in parties:
@@ -59,7 +95,7 @@ drops = ['1','2','3','4','5']
 d = {}
 for i in range(1):
   # i=j+1
-  d[i]=pd.DataFrame(df[2])
+  d[i]=pd.DataFrame(df[3])
   d[i].columns = headers
   d[i]=d[i].drop(drops, axis=1)
   for z in parties:
--- Polish/President/poll_new.R
+++ Polish/President/poll_new.R
@@ -11,8 +11,7 @@ library(svglite)
 library(Rcpp)
 library(ggpubr)
 
-py_run_file("Polish/President/data.py")
-poll <- read_csv("Polish/President/poll.csv")
+poll <- read_csv("Polish/President/poll_new.csv")
 d <- reshape2::melt(poll, id.vars="Date")
 d$value<-as.numeric(d$value)/100
 d$value<-formattable::percent(d$value)
@@ -20,34 +19,23 @@ d$value<-formattable::percent(d$value)
 old<-min(d$Date)
 election<-as.Date("18 05 2025", "%d %m %Y")
 # election<-max(d$Date)+14
-new<-d[d$variable!='Mentzen'&d$variable!='Nawrocki'&d$variable!='Bosak',]
-new2<-d[d$variable=='Nawrocki',]
-new3<-d[d$variable=='Mentzen',]
-new4<-d[d$variable=='Bosak',]
-new2<-new2[!is.na(new2$value),]
-new3<-new3[!is.na(new3$value),]
-new4<-new4[!is.na(new4$value),]
+
 # MAIN GRAPH
 
-colss <-c("Morawiecki"        ="#979fbe",
-          "Nawrocki"          ="#263778",
-          "Trzaskowski"       ="#F68F2D",
-          "Hołownia"          ="#F9C013",
-          "Dziemianowicz-Bąk" ="#eb2a48",
-          "Biejat"            ="#870f57",
-          "Bosak"             ="#122746",
-          "Mentzen"           ="#717d90")
+colss <-c(
+          "Nawrocki"   ="#263778",
+          "Trzaskowski"="#F68F2D",
+          "Hołownia"   ="#F9C013",
+          "Biejat"     ="#eb2a48",
+          "Mentzen"    ="#717d90",
+          "Jakubiak"   ="#c99999")
 
 # LOESS GRAPH
 
 plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
-  geom_point(size=1, data=d[d$Date!=old,],alpha=0.5)+
+  geom_point(size=1.2, data=d[d$Date!=election,],alpha=0.5)+
   scale_color_manual(values = colss)+
-  # geom_smooth(method="loess",fullrange=FALSE,se=FALSE,span=0.5,linewidth=0.75, data=d[d$Date!=election,])+
-  geom_smooth(method="loess",fullrange=FALSE,se=FALSE,span=0.5,linewidth=0.75, data=new[new$Date!=election,])+
-  geom_smooth(method="loess",fullrange=FALSE,se=FALSE,span=1,linewidth=0.75, data=new2[new2$Date!=old,])+
-  geom_smooth(method="loess",fullrange=FALSE,se=FALSE,span=0.7,linewidth=0.75, data=new3[new3$Date!=election,])+
-  geom_smooth(method="loess",fullrange=FALSE,se=FALSE,span=0.7,linewidth=0.75, data=new4[new4$Date!=election,])+
+  geom_smooth(method="loess",fullrange=FALSE,se=FALSE,span=1,linewidth=0.75, data=d[d$Date!=election,])+
   theme_minimal()+
   theme(axis.title=element_blank(),legend.title = element_blank(),
         legend.key.size = unit(2, 'lines'),
@@ -67,18 +55,18 @@ plot1<-ggplot(data=d,aes(x=Date,y=value, colour=variable, group=variable)) +
   geom_point(data=d[d$Date==election,],size=5, shape=18, alpha=0.5)+
   geom_point(data=d[d$Date==election,],size=5.25, shape=5, alpha=0.5)+
   geom_hline(aes(yintercept=0), alpha=0, linetype="longdash", colour="#000000")+
-  scale_x_date(date_breaks = "2 months", date_labels =  "%b %Y",limits = c(old,election),guide = guide_axis(angle = -90))+
+  scale_x_date(date_breaks = "1 week", date_labels =  "%d %b %Y",limits = c(old,election),guide = guide_axis(angle = -90))+
   ggtitle('Opinion Polling for the Next Polish Presidential Election')
 plot1
 
 
 
-poll <- read_csv("Polish/President/poll.csv")
+poll <- read_csv("Polish/President/poll_new.csv")
 # poll$Date <- as.Date(poll$Date, "%d %b %Y")
 Date <- c(max(poll$Date))
 poll[-1]<-data.frame(apply(poll[-1], 2, function(x) 
   as.numeric(sub("%","",as.character(x)))))
-poll<-poll[poll$Date>(max(poll$Date)-1),]
+poll<-poll[poll$Date>(max(poll$Date)-7),]
 d1 <- colMeans(poll[-1],na.rm=TRUE)
 d1 <- as.data.frame(d1)
 d1 <- t(d1)
@@ -91,17 +79,17 @@ d1$value<-formattable::percent(d1$value, digits = 1)
 
 plot2<-ggplot(data=d1, aes(x=variable, y=value,fill=interaction(Date,variable), group=Date )) +
   geom_bar(stat="identity",width=0.9, position=position_dodge())+
-  scale_fill_manual(values = c("#979fbe","#263778","#515f93","#F68F2D","#F9C013","#eb2a48","#870f57","#122746","#717d90"))+
+  scale_fill_manual(values = c("#263778","#F68F2D","#F9C013","#eb2a48","#717d90","#c99999"))+
   geom_text(aes(label = formattable::percent(d1$value, digits = 1),y = 0),
             hjust=-0.1, color="#000000",position = position_dodge(1), size=3.5, fontface="bold.italic")+
   theme_minimal()+
   theme(legend.position = "none",axis.title=element_blank(),axis.text.x = element_blank(),
-        axis.text.y = element_text(face="bold.italic"),
+        axis.text.y = element_text(face="bold.italic",colour="#000000"),
         plot.title = element_text(face="bold.italic"),
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_rect(fill="#FFFFFF",color="#FFFFFF"),
         plot.background = element_rect(fill = "#FFFFFF",color="#FFFFFF"))+
-  ggtitle('Latest Poll')+
+  ggtitle('7 Day Average')+
   scale_x_discrete(limits = d1$variable[order(d1$value,na.last = FALSE)])+
   coord_flip()
 plot2
@@ -109,4 +97,4 @@ plot2
 
 plot<-ggarrange(plot1, plot2,ncol = 2, nrow = 1,widths=c(2,0.5))
 plot
-ggsave(plot=plot, file="Polish/President/plot.png",width = 15, height = 7.5, type = "cairo-png")
+ggsave(plot=plot, file="Polish/President/plot_new.png",width = 15, height = 7.5, type = "cairo-png")
